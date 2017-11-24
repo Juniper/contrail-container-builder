3f4a5e1a3e1e5fce1ddbd01b01b4825aa14fcff1
Feodor Tersin <ftersin@hotmail.com>
2017-11-24 15:06:41 +0000

    apply & delete scripts to operate with manifest at once

---
+++ kubernetes/manifests/apply.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+manifest_dir="${BASH_SOURCE%/*}"
+if [[ ! -d "$manifest_dir" ]]; then manifest_dir="$PWD"; fi
+
+template_file=${1:-$manifest_dir"/contrail-template.yaml"}
+
+$manifest_dir/resolve-manifest.sh "$template_file" | kubectl apply -f -
