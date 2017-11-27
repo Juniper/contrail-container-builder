a8f5a48d0b42b0ff3ef1726b0a8d647291a390d3
Andrey Pavlov <andrey.mp@gmail.com>
2017-11-27 21:45:25 +0300

    revert repo_dir definition to unpack-vrouter script

--- containers/unpack-vrouter-module.sh
+++ containers/unpack-vrouter-module.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+if [ -n $CONTRAIL_REPOSITORY ]; then
+  dir_prefix=$(echo $CONTRAIL_REPOSITORY | awk -F'/' '{print $4}' | sed 's/'$version'$//')
+fi
+repo_dir="${package_root_dir}/${dir_prefix}${contrail_version}"
+
 tmp=$(mktemp -d)
 pushd $tmp
 rpm2cpio "$repo_dir/contrail-vrouter-${CONTRAIL_VERSION}.el7.x86_64.rpm" | cpio -idmv
