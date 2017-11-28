ce0118e2bc718439ae85ad63bed56869ec82e890
Andrey Pavlov <andrey.mp@gmail.com>
2017-11-28 19:29:49 +0300

    add downloading hack to build-repository
    download third-party-packages if build_packages is set

--- containers/build-repository.sh
+++ containers/build-repository.sh
@@ -5,9 +5,13 @@ echo "INFO: BUILD_PACKAGES is true - run build..."
 # all paths are hardcoded here...
 $HOME/contrail-build-poc/build.sh
 
+sudo yum install -y createrepo
+
 cp $HOME/rpmbuild/RPMS/x86_64/*.rpm $repo_dir/
 cp $HOME/rpmbuild/RPMS/noarch/*.rpm $repo_dir/
 pushd $repo_dir
-sudo yum install -y createrepo
+wget https://s3-us-west-2.amazonaws.com/contrailrhel7/third-party-packages.tgz
+tar -xf third-party-packages.tgz
+rm third-party-packages.tgz
 sudo createrepo .
 popd
