cc0cc6d282d734b7e10a5ec6ef78c06edb73a21e
alexey-mr <alexey.morlang@gmail.com>
2017-12-02 17:33:18 +0300

    k8s: kafka: use own IP as  advertised.host.name

--- containers/external/kafka/docker-entrypoint.sh
+++ containers/external/kafka/docker-entrypoint.sh
@@ -59,17 +59,19 @@ KAFKA_log_cleaner_dedupe_buffer_size=${KAFKA_log_cleaner_dedupe_buffer_size:-250
 KAFKA_log_cleaner_enable=${KAFKA_log_cleaner_enable:-true}
 KAFKA_delete_topic_enable=${KAFKA_delete_topic_enable:-true}
 
-sed -i "s/^#log.retention.bytes=.*$/log.retention.bytes=$KAFKA_log_retention_bytes/g" ${CONFIG}
-sed -i "s/^log.segment.bytes=.*$/log.retention.bytes=$KAFKA_log_segment_bytes/g" ${CONFIG}
-sed -i "s/^log.retention.hours=.*$/log.retention.hours=$KAFKA_log_retention_hours/g" ${CONFIG}
-sed -i "s)^zookeeper.connect=.*$)zookeeper.connect=$zk_list)g" ${CONFIG}
-sed -i "s/^num.partitions=.*$/num.partitions=30/g" ${CONFIG}
-sed -i "s/^num.partitions=.*$/num.partitions=30/g" ${CONFIG}
 sed -i "s/^broker.id=.*$/broker.id=$KAFKA_BROKER_ID/g" ${CONFIG}
+sed -i "s/#port=.*$/port=$KAFKA_LISTEN_PORT/g" ${CONFIG}
 sed -i "s/^listeners=.*$/listeners=PLAINTEXT:\/\/$KAFKA_LISTEN_ADDRESS:$KAFKA_LISTEN_PORT/g" ${CONFIG}
-echo "default.replication.factor=$replication_factor" >> ${CONFIG}
+sed -i "s)^zookeeper.connect=.*$)zookeeper.connect=$zk_list)g" ${CONFIG}
+sed -i "s/#advertised.host.name=.*$/advertised.host.name=$my_ip/g" ${CONFIG}
+sed -i "s/^#log.retention.bytes=.*$/log.retention.bytes=$KAFKA_log_retention_bytes/g" ${CONFIG}
+sed -i "s/^log.retention.hours=.*$/log.retention.hours=$KAFKA_log_retention_hours/g" ${CONFIG}
+sed -i "s/^log.segment.bytes=.*$/log.segment.bytes=$KAFKA_log_segment_bytes/g" ${CONFIG}
 echo "log.cleanup.policy=${KAFKA_log_cleanup_policy}" >> ${CONFIG}
 echo "log.cleaner.threads=${KAFKA_log_cleaner_threads}" >> ${CONFIG}
 echo "log.cleaner.dedupe.buffer.size=${KAFKA_log_cleaner_dedupe_buffer_size}" >> ${CONFIG}
+sed -i "s/^num.partitions=.*$/num.partitions=30/g" ${CONFIG}
+sed -i "s/^num.partitions=.*$/num.partitions=30/g" ${CONFIG}
+echo "default.replication.factor=$replication_factor" >> ${CONFIG}
 
 exec "$@"
