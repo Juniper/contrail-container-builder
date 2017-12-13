a46b81e9d2c63a34cc83fcec2c14e68afc8b1e15
alexey-mr <alexey.morlang@gmail.com>
2017-12-13 17:22:47 +0300

    Rename rabbit nodes from rabbit@<name> to contrail@<name>

--- containers/external/rabbitmq-init/contrail-entrypoint.sh
+++ containers/external/rabbitmq-init/contrail-entrypoint.sh
@@ -5,7 +5,7 @@ IFS="," read -ra srv_list <<< "$RABBITMQ_NODES"
 local_ips=$(cat "/proc/net/fib_trie" | awk '/32 host/ { print f } {f=$2}')
 for srv in "${srv_list[@]}"; do
   node_name="node-"$(echo $srv | tr '.' '-')
-  cluster_nodes+="'rabbit@$node_name',"
+  cluster_nodes+="'contrail@$node_name',"
   echo $srv $node_name >> /etc/hosts
   if [[ "$local_ips" =~ "$srv" ]] ; then
     echo "INFO: found '$srv' in local IPs '$local_ips'"
@@ -20,7 +20,7 @@ if [ -z "$my_ip" ]; then
   exit
 fi
 
-export RABBITMQ_NODENAME=rabbit@$my_node
+export RABBITMQ_NODENAME=contrail@$my_node
 if (( ${#srv_list[@]} > 1 )); then
   export RABBITMQ_SERVER_ADDITIONAL_ERL_ARGS="-rabbit cluster_nodes $cluster_nodes"
 fi
