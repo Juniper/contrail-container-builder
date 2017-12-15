c8e9409d0968e976bb6f90717eb90beca379f9ec
root <michael.henkel@gmail.com>
2017-12-15 04:50:17 -0800

    fixed zk

--- containers/external/zookeeper/contrail-entrypoint.sh
+++ containers/external/zookeeper/contrail-entrypoint.sh
@@ -13,11 +13,12 @@ done
 for srv in "${srv_list[@]}"; do
   if [[ "$local_ips" =~ "$srv" ]] ; then
     echo "INFO: found '$srv' in local IPs '$local_ips'"
+    my_ip=$srv
     break
   fi
 done
 
-if (( $ord < 1 || $ord > "${#srv_list[@]}" )); then
+if [ -z "$my_ip" ]; then
   echo "ERROR: Cannot find self ips ('$local_ips') in Zookeeper nodes ('$ZOOKEEPER_NODES')"
   exit
 fi
