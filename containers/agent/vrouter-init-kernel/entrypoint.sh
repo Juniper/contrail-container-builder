eb6907f3338f6baefcfe8379e6351af8e527d931
Andrey Pavlov <andrey.mp@gmail.com>
2018-01-16 23:00:18 +0300

    add vrouter.ko compliling for ubuntu
    Change-Id: I1603fc81608630831107a90247107df2e231745f
    Partial-Bug: #1743644

--- containers/agent/vrouter-init-kernel/entrypoint.sh
+++ containers/agent/vrouter-init-kernel/entrypoint.sh
@@ -1,30 +1,56 @@
 #!/bin/bash
 
+# these next folders must be mounted to compile vrouter.ko in ubuntu: /usr/src /lib/modules
+
 source /common.sh
 
-echo "INFO: ip address show:"
-ip address show
-
-IFS=' ' read -r phys_int phys_int_mac <<< $(get_physical_nic_and_mac)
-echo "INFO: Physical interface: $phys_int, mac=$phys_int_mac"
-
-# Probe vhost0
-vrouter_cidr="$(get_cidr_for_nic vhost0)"
-
 # Load kernel module
-kver=`uname -r | awk -F "-" '{print $1}'`
-echo "INFO: Load kernel module for kver=$kver"
-modfile=`ls -1rt /opt/contrail/vrouter-kernel-modules/$kver-*/vrouter.ko | tail -1`
-if ! lsmod | grep -q vrouter; then
-    echo "INFO: Modprobing vrouter "$modfile
+if lsmod | grep -q vrouter; then
+  echo "INFO: vrouter.ko already loaded in the system"
+  # TODO: handle upgrade
+else
+  kver=`uname -r | awk -F "-" '{print $1}'`
+  echo "INFO: Load kernel module for kver=$kver"
+  linux=$(awk -F"=" '/^ID=/{print $2}' /etc/os-release | tr -d '"')
+  echo "INFO: detected linux id: $linux"
+  if [[ "$linux" == 'ubuntu' ]]; then
+    echo "INFO: Compiling vrouter kernel module for ubuntu..."
+    if [ ! -d "/usr/src/linux-headers-$kver" ] ; then
+      echo "ERROR: There is no kernel headers in /usr/src for current kernel. Exiting..."
+      exit 1
+    fi
+
+    vrouter_full_ver=`dpkg -l contrail-vrouter-dkms | awk '/contrail-vrouter-dkms/{print $3}'`
+    vrouter_release_ver=`echo $vrouter_full_ver | cut -d '-' -f 1`
+    echo "INFO: detected vrouter version is $vrouter_ver"
+    # copy vrouter sources to /usr/src using full version
+    mkdir -p /usr/src/vrouter-$vrouter_full_ver
+    cp -r /opt/contrail/src/vrouter-$vrouter_release_ver/* /usr/src/vrouter-$vrouter_full_ver
+    # and make correct link to correct build
+    ln -s /usr/src/vrouter-$vrouter_full_ver /usr/src/vrouter-$vrouter_release_ver
+    # build it
+    dpkg-reconfigure contrail-vrouter-dkms
+    depmod -a
+    touch /usr/src/vrouter-$vrouter_full_ver/module_compiled
+    modfile="/lib/modules/$kver/updates/dkms/vrouter.ko"
+  elif [[ "$linux" == 'centos' ]] ; then
+    modfile=`ls -1rt /opt/contrail/vrouter-kernel-modules/$kver-*/vrouter.ko | tail -1`
+  else
+    echo "ERROR: Unsupported linux distribution"
+    exit 1
+  fi
+
+  echo "INFO: Modprobing vrouter $modfile"
+  insmod $modfile
+  if ! lsmod | grep -q vrouter ; then
+    echo "WARNING: Failed to insert vrouter kernel module. Trying to drop caches and insert it again."
+    free -h && sync && echo 2 >/proc/sys/vm/drop_caches && free -h
     insmod $modfile
     if ! lsmod | grep -q vrouter ; then
-        echo "ERROR: Failed to insert vrouter kernel module"
-        exit 1
+      echo "ERROR: Failed to insert vrouter kernel module"
+      exit 1
     fi
-else
-    echo "INFO: vrouter.ko already loaded in the system"
-    # TODO: handle upgrade
+  fi
 fi
 
 # VRouter specific code starts here
@@ -63,6 +89,15 @@ function insert_vrouter() {
     ip link set vhost0 up
     return 0
 }
+
+echo "INFO: ip address show:"
+ip address show
+
+IFS=' ' read -r phys_int phys_int_mac <<< $(get_physical_nic_and_mac)
+echo "INFO: Physical interface: $phys_int, mac=$phys_int_mac"
+
+# Probe vhost0
+vrouter_cidr="$(get_cidr_for_nic vhost0)"
 
 if [[ "$vrouter_cidr" == '' ]] ; then
     echo "INFO: creating vhost0"
