acb24914605cd99507c0aae478880115f58af0b1
Harsh Kumar <hkumar@juniper.net>
2018-01-16 22:37:37 +0530

    Initial commit for dpdk containerization
    Change-Id: I151b037f140e4ec4ca99713ce2bbe25d3beed9ba
    Partial-Bug: #1743610

---
+++ containers/agent/vrouter-dpdk/entrypoint.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+source /common.sh
+
+HYPERVISOR_TYPE=${HYPERVISOR_TYPE:-'kvm'}
+VROUTER_HOSTNAME=${VROUTER_HOSTNAME:-${DEFAULT_HOSTNAME}}
+
+IFS=' ' read -r phys_int phys_int_mac <<< $(get_physical_nic_and_mac)
+echo "INFO: Physical interface: $phys_int, mac=$phys_int_mac"
+
+# It is expected that vhost0 is up and running here
+VROUTER_GATEWAY=${VROUTER_GATEWAY:-`get_default_gateway_for_nic vhost0`}
+vrouter_cidr=$(get_cidr_for_nic vhost0)
+echo "INFO: vhost0 cidr $vrouter_cidr, gateway $VROUTER_GATEWAY"
+
+# It is expected that vhost0 is up and running here
+if [[ -z "$vrouter_cidr" ]] ; then
+    echo "ERROR: vhost0 interface is down or has no assigned IP"
+    exit -1
+fi
+vrouter_ip=${vrouter_cidr%/*}
+
+if [[ -z "$VROUTER_GATEWAY" ]] ; then
+    echo "ERROR: VROUTER_GATEWAY is empty or there is no default route for vhost0"
+    exit -1
+fi
+
+mkdir -p -m 777 /var/crashes
+
+
+# Prepare agent configs
+echo "INFO: Preparing /etc/contrail/contrail-vrouter-agent.conf"
+cat << EOM > /etc/contrail/contrail-vrouter-agent.conf
+[CONTROL-NODE]
+servers=${XMPP_SERVERS:-`get_server_list CONTROL ":$XMPP_SERVER_PORT "`}
+
+[DEFAULT]
+collectors=$COLLECTOR_SERVERS
+log_file=${VROUTER_LOG_FILE:-"$LOG_DIR/contrail-vrouter-agent.log"}
+log_level=${VROUTER_LOG_LEVEL:-$LOG_LEVEL}
+log_local=${VROUTER_LOG_LOCAL:-$LOG_LOCAL}
+
+xmpp_dns_auth_enable = False
+xmpp_auth_enable = False
+physical_interface_mac = $phys_int_mac
+platform = dpdk
+
+[SANDESH]
+introspect_ssl_enable = False
+sandesh_ssl_enable = False
+
+[NETWORKS]
+control_network_ip=$vrouter_ip
+
+[DNS]
+servers=${DNS_SERVERS:-`get_server_list CONTROL ":$DNS_SERVER_PORT "`}
+
+[METADATA]
+metadata_proxy_secret=${METADATA_PROXY_SECRET}
+
+[VIRTUAL-HOST-INTERFACE]
+name=vhost0
+ip=$vrouter_cidr
+physical_interface=$phys_int
+gateway=$VROUTER_GATEWAY
+compute_node_address=$vrouter_ip
+
+[SERVICE-INSTANCE]
+netns_command=/usr/bin/opencontrail-vrouter-netns
+docker_command=/usr/bin/opencontrail-vrouter-docker
+
+[HYPERVISOR]
+type = $HYPERVISOR_TYPE
+EOM
+
+set_vnc_api_lib_ini
+
+# Prepare default_pmac
+echo $phys_int_mac > /etc/contrail/default_pmac
+
+wait_for_contrail_api
+
+provision_node provision_vrouter.py $vrouter_ip $VROUTER_HOSTNAME
+
+exec "$@"
