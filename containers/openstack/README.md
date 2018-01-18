15d6307d840c2f187e58948990c6ef2b18480793
Andrey Pavlov <andrey.mp@gmail.com>
2018-01-18 13:06:38 +0300

    add heat-init container
    Change-Id: Ib857dc2c5c9ad94eec5f3095791b524734e7fd5f
    Closes-Bug: #1744020

--- containers/openstack/README.md
+++ containers/openstack/README.md
@@ -2,5 +2,6 @@
 
 - compute-init
 - neutron-init
+- heat-init
 
-They bring on the host all Cotnrailer tools that are required by appropriate Openstack services.
+They bring on the host all Contrail tools that are required by appropriate Openstack services.
--- containers/openstack/heat-init/entrypoint.sh
+++ containers/openstack/heat-init/entrypoint.sh
@@ -14,7 +14,7 @@ else
 fi
 
 mkdir -p /opt/plugin/site-packages
-for module in neutron_plugin_contrail vnc_api cfgm_common neutron_lbaas ; do
+for module in contrail_heat vnc_api cfgm_common ; do
   for item in `ls -d $src_path/${module}*` ; do
     cp -r $item /opt/plugin/site-packages/
   done
