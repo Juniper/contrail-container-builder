9d6d7d37f01b6279385da9737027e3f7a26d170a
nitishkrishna <nitishk@juniper.net>
2018-02-05 13:30:25 -0800

    Partial-Bug: #1736197 - New mircoservice container for Ironic Notification Daemon
    The container runs the ironic notification manager service
    Package ironic-notification-manager gets installed
    Patch:
    
    Moved to openstack folder, Analytics role
    Use full names, no need of docker compose
    Removed Ironic server, port (not needed)
    Moved Run code
    
    Change-Id: I4574ccb4ae61a3492df900708906b90b1aa8fb27

---
+++ containers/openstack/ironic-notification-manager/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source /common.sh
+
+cat > /etc/contrail/ironic-notification-manager.conf << EOM
+[DEFAULTS]
+log_file = ${IRONIC_NOTIFICATION_MANAGER_LOG_FILE:-"$LOG_DIR/ironic-notification-manager.log"}
+log_level = ${IRONIC_NOTIFICATION_MANAGER_LOG_LEVEL:-$LOG_LEVEL}
+log_local = 1
+rabbit_server = $RABBITMQ_SERVERS
+rabbit_port = $RABBITMQ_NODE_PORT
+rabbit_user = $RABBITMQ_USER
+rabbit_password = $RABBITMQ_PASSWORD
+notification_level = $IRONIC_NOTIFICATION_LEVEL
+collectors = $COLLECTOR_SERVERS
+introspect_port = $IRONIC_NOTIFICATION_MANAGER_INTROSPECT_PORT
+
+EOM
+
+set_third_party_auth_config
+
+exec "$@"
