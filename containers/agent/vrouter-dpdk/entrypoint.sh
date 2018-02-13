ce00b0d92a60b6d118c09d74e8b9861e981172e4
alexey-mr <alexey.morlang@gmail.com>
2018-02-13 21:02:25 +0300

    Dpdk - move vhost creation into agent container.
    Dpdk init container now creates files in /var/run/vrouter
    that are consumed in agent container to up vhost0.
    
    Change-Id: I5051b7c1cf123aee0f2b875be1e6c59b5c736486
    Partial-Bug: #1743610

--- containers/agent/vrouter-dpdk/entrypoint.sh
+++ containers/agent/vrouter-dpdk/entrypoint.sh
@@ -3,13 +3,15 @@
 source /common.sh
 
 echo "INFO: agent started in $AGENT_MODE mode"
-IFS=' ' read -r phys_int phys_int_mac <<< $(get_physical_nic_and_mac)
-pci_address=$(get_pci_address_for_nic $phys_int)
+
+phys_int=`get_default_physical_iface`
+binding_data_dir="/var/run/vrouter/"
+phys_int_mac=`cat $binding_data_dir/${phys_int}_mac`
+pci_address=`cat $binding_data_dir/${phys_int}_pci`
 echo "INFO: Physical interface: $phys_int, mac=$phys_int_mac, pci=$pci_address"
 
-VROUTER_GATEWAY=${VROUTER_GATEWAY:-`get_default_gateway_for_nic $phys_int`}
-vrouter_cidr=$(get_cidr_for_nic $phys_int)
-echo "INFO: $phys_int cidr $vrouter_cidr, gateway $VROUTER_GATEWAY"
+# ensure device is bind to dpdk driver
+wait_device_for_driver $DPDK_UIO_DRIVER $pci_address
 
 # TODO: consider to avoid taskset here and leave to manage by Docker
 cmd="$@"
@@ -33,23 +35,7 @@ mkdir -p -m 777 /var/crashes
 rm -f '/run/.rte_config'
 
 # set maximum socket buffer size to (max hold flows entries * 9160 bytes)
-sysctl -w net.core.wmem_max=9160000
-
-# bind iface to dpdk uio driver before start dpdk agent
-bind_dev_to_driver $DPDK_UIO_DRIVER $phys_int
-
-function background_init() {
-    wait_dpdk_agent_start
-    if [[ -n $cmd ]] ; then
-        local pname=`echo $cmd | cut -d ' ' -f 1`
-        enable_hugepages_to_coredump "$pname"
-    fi
-    create_vhost0_dpdk $phys_int $phys_int_mac $vrouter_cidr $VROUTER_GATEWAY
-}
-
-# TODO: tart backgound job to enable hugepase to coredump
-# and initialize vhost0
-background_init &
+set_ctl net.core.wmem_max 9160000
 
 echo "INFO: exec '$real_cmd'"
 exec $real_cmd
