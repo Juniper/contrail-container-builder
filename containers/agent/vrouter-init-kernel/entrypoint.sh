80fb8341f421e0951e68e17ddd346d495409689f
Andrey Pavlov <andrey.mp@gmail.com>
2018-02-13 16:38:27 +0300

    remove building ubuntu images
    Change-Id: Ibbc2f7195c0bbdf7d2064b890f8ae0078fca5a02
    Closes-Bug: #1748957

--- containers/agent/vrouter-init-kernel/entrypoint.sh
+++ containers/agent/vrouter-init-kernel/entrypoint.sh
@@ -12,26 +12,8 @@ else
   linux=$(awk -F"=" '/^ID=/{print $2}' /etc/os-release | tr -d '"')
   echo "INFO: detected linux id: $linux"
   if [[ "$linux" == 'ubuntu' ]]; then
-    echo "INFO: Compiling vrouter kernel module for ubuntu..."
+    # TODO: check vrouter.ko presense in the system
     kver=`uname -r`
-    echo "INFO: Load kernel module for kver=$kver"
-    if [ ! -d "/usr/src/linux-headers-$kver" ] ; then
-      echo "ERROR: There is no kernel headers in /usr/src for current kernel. Exiting..."
-      exit 1
-    fi
-
-    vrouter_full_ver=`dpkg -l contrail-vrouter-dkms | awk '/contrail-vrouter-dkms/{print $3}'`
-    vrouter_release_ver=`echo $vrouter_full_ver | cut -d '-' -f 1`
-    echo "INFO: detected vrouter version is $vrouter_ver"
-    # copy vrouter sources to /usr/src using full version
-    mkdir -p /usr/src/vrouter-$vrouter_full_ver
-    cp -r /opt/contrail/src/vrouter-$vrouter_release_ver/* /usr/src/vrouter-$vrouter_full_ver
-    # and make correct link to correct build
-    ln -s /usr/src/vrouter-$vrouter_full_ver /usr/src/vrouter-$vrouter_release_ver
-    # build it
-    dpkg-reconfigure contrail-vrouter-dkms
-    depmod -a
-    touch /usr/src/vrouter-$vrouter_full_ver/module_compiled
     modfile="/lib/modules/$kver/updates/dkms/vrouter.ko"
   elif [[ "$linux" == 'centos' ]] ; then
     kver=`uname -r | awk -F "-" '{print $1}'`
