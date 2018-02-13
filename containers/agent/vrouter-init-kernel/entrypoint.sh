70dceecc8a0dd1ea0d6562797d01179d54281229
Andrey Pavlov <andrey.mp@gmail.com>
2018-02-13 16:38:27 +0300

    remove building ubuntu images
    Change-Id: Ibbc2f7195c0bbdf7d2064b890f8ae0078fca5a02
    Depends-On: I26489ecf7699e02486d8885b5827ef4e893bd22b
    Closes-Bug: #1748957

--- containers/agent/vrouter-init-kernel/entrypoint.sh
+++ containers/agent/vrouter-init-kernel/entrypoint.sh
@@ -9,49 +9,14 @@ if lsmod | grep -q vrouter; then
   echo "INFO: vrouter.ko already loaded in the system"
   # TODO: handle upgrade
 else
-  linux=$(awk -F"=" '/^ID=/{print $2}' /etc/os-release | tr -d '"')
-  echo "INFO: detected linux id: $linux"
-  if [[ "$linux" == 'ubuntu' ]]; then
-    echo "INFO: Compiling vrouter kernel module for ubuntu..."
-    kver=`uname -r`
-    echo "INFO: Load kernel module for kver=$kver"
-    if [ ! -d "/usr/src/linux-headers-$kver" ] ; then
-      echo "ERROR: There is no kernel headers in /usr/src for current kernel. Exiting..."
-      exit 1
-    fi
-
-    vrouter_full_ver=`dpkg -l contrail-vrouter-dkms | awk '/contrail-vrouter-dkms/{print $3}'`
-    vrouter_release_ver=`echo $vrouter_full_ver | cut -d '-' -f 1`
-    echo "INFO: detected vrouter version is $vrouter_ver"
-    # copy vrouter sources to /usr/src using full version
-    mkdir -p /usr/src/vrouter-$vrouter_full_ver
-    cp -r /opt/contrail/src/vrouter-$vrouter_release_ver/* /usr/src/vrouter-$vrouter_full_ver
-    # and make correct link to correct build
-    ln -s /usr/src/vrouter-$vrouter_full_ver /usr/src/vrouter-$vrouter_release_ver
-    # build it
-    dpkg-reconfigure contrail-vrouter-dkms
-    depmod -a
-    touch /usr/src/vrouter-$vrouter_full_ver/module_compiled
-    modfile="/lib/modules/$kver/updates/dkms/vrouter.ko"
-  elif [[ "$linux" == 'centos' ]] ; then
-    kver=`uname -r | awk -F "-" '{print $1}'`
-    echo "INFO: Load kernel module for kver=$kver"
-    modfile=`ls -1rt /opt/contrail/vrouter-kernel-modules/$kver-*/vrouter.ko | tail -1`
-  else
-    echo "ERROR: Unsupported linux distribution"
-    exit 1
-  fi
-
+  kver=`uname -r | awk -F "-" '{print $1}'`
+  echo "INFO: Load kernel module for kver=$kver"
+  modfile=`ls -1rt /opt/contrail/vrouter-kernel-modules/$kver-*/vrouter.ko | tail -1`
   echo "INFO: Modprobing vrouter $modfile"
-  insmod $modfile
-  if ! lsmod | grep -q vrouter ; then
-    echo "WARNING: Failed to insert vrouter kernel module. Trying to drop caches and insert it again."
-    free -h && sync && echo 2 >/proc/sys/vm/drop_caches && free -h
-    insmod $modfile
-    if ! lsmod | grep -q vrouter ; then
-      echo "ERROR: Failed to insert vrouter kernel module"
-      exit 1
-    fi
+  free -h && sync && echo 2 >/proc/sys/vm/drop_caches && free -h
+  if ! insmod $modfile ; then
+    echo "ERROR: Failed to insert vrouter kernel module"
+    exit 1
   fi
 fi
 
