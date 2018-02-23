e0bc59b4e5734cb514713d86b9536968aa120cfe
Amudha <amudha@juniper.net>
2018-02-23 13:10:26 -0800

    vCenter: create microservice container for vcenter-plugin
    Change-Id: I2fae11006491634c3481e8b6760acd13fffa694f
    Partial-BUg: #1751339

---
+++ containers/vcenter-plugin/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source /common.sh
+
+cat > /etc/contrail/contrail-vcenter-plugin.conf << EOM
+[DEFAULT]
+# Vcenter plugin URL
+vcenter.url = ${CONTRAIL_VCENTER_URL:-$VCENTER_SERVER}
+
+#Vcenter credentials
+vcenter.username = ${CONTRAIL_VCENTER_USERNAME:-$VCENTER_USERNAME}
+vcenter.password = ${CONTRAIL_VCENTER_PASSWORD:-$VCENTER_PASSWORD}
+
+vcenter.datacenter = ${CONTRAIL_VCENTER_DATACENTER:-$VCENTER_DATACENTER}
+vcenter.dvswitch = ${CONTRAIL_VCENTER_CLUSTER:-$VCENTER_CLUSTER}
+vcenter.ipfabricpg = ${CONTRAIL_VCENTER_IPFABRICPG:-$VCENTER_IPFABRICPG}
+
+api.hostname = ${CONTRAIL_API_SERVER:-$CONFIG_SERVERS}
+api.port = ${CONTRAIL_API_PORT:-8082}
+
+zookeeper.serverlist = ${CONTRAIL_ANALYTICS_NODES}:2181
+
+introspect_port = ${CONTRAIL_VCENTER_PLUGIN_INTROSPECT_PORT:-8110}
+
+EOM
+
+set_vnc_api_lib_ini
+
+wait_for_contrail_api
+
+exec "$@"
