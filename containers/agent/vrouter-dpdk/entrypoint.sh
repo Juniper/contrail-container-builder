9611fbcb8b95978d589c6753a4d899bbb4bfdad1
alexey-mr <alexey.morlang@gmail.com>
2018-02-26 16:58:26 +0300

    Vlan & bonding support
    Change-Id: Ib8ad3e164b984f925593e89884e2fd7bd6d74c14
    Partial-Bug: #1750797

--- containers/agent/vrouter-dpdk/entrypoint.sh
+++ containers/agent/vrouter-dpdk/entrypoint.sh
@@ -3,7 +3,18 @@
 source /common.sh
 source /agent-functions.sh
 
+DPDK_MEM_PER_SOCKET=${DPDK_MEM_PER_SOCKET:-1024}
+
 echo "INFO: agent started in $AGENT_MODE mode"
+
+mkdir -p -m 777 /var/crashes
+
+# remove rte configuration file (for case if vRouter has crashed)
+# TODO: most probably not needed.. since crash means container re-created
+rm -f '/run/.rte_config'
+
+# set maximum socket buffer size to (max hold flows entries * 9160 bytes)
+set_ctl net.core.wmem_max 9160000
 
 function assert_file() {
     local file=$1
@@ -20,29 +31,65 @@ phys_int_mac=`cat "$binding_data_dir/${phys_int}_mac"`
 assert_file "$binding_data_dir/${phys_int}_pci"
 pci_address=`cat "$binding_data_dir/${phys_int}_pci"`
 echo "INFO: Physical interface: $phys_int, mac=$phys_int_mac, pci=$pci_address"
+vlan_data=''
+if [[ -f "$binding_data_dir/${phys_int}_vlan" ]] ; then
+    vlan_data=$(cat "$binding_data_dir/${phys_int}_vlan")
+    echo "INFO: vlan_data: $vlan_data"
+fi
+bond_data=''
+if [[ -f "$binding_data_dir/${phys_int}_bond" ]] ; then
+    bond_data=$(cat "$binding_data_dir/${phys_int}_bond")
+    echo "INFO: bond_data: $bond_data"
+fi
 
-# ensure device is bind to dpdk driver
-wait_device_for_driver $DPDK_UIO_DRIVER $pci_address
 
+# base command
+cmd="$@ --no-daemon"
+
+# update command with taskset options (core mask)
 # TODO: consider to avoid taskset here and leave to manage by Docker
-cmd="$@"
-real_cmd=$cmd
 if [[ -n "$CPU_CORE_MASK" ]] ; then
     taskset_param="$CPU_CORE_MASK"
     if [[ "${CPU_CORE_MASK}" =~ [,-] ]]; then
         taskset_param="-c $CPU_CORE_MASK"
     fi
-    real_cmd="/bin/taskset $taskset_param $cmd"
+    cmd="/bin/taskset $taskset_param $cmd"
 fi
 
-mkdir -p -m 777 /var/crashes
+# update command with socket mem option
+dpdk_socket_mem=''
+for _ in /sys/devices/system/node/node*/hugepages ; do
+    if [[ -z "${dpdk_socket_mem}" ]] ; then
+        dpdk_socket_mem="${DPDK_MEM_PER_SOCKET}"
+    else
+        dpdk_socket_mem+=",${DPDK_MEM_PER_SOCKET}"
+    fi
+done
+[ -z "${dpdk_socket_mem}" ] && dpdk_socket_mem="${DPDK_MEM_PER_SOCKET}"
+cmd+=" --socket-mem $dpdk_socket_mem"
 
-# remove rte configuration file (for case if vRouter has crashed)
-# TODO: most probably not needed.. since crash means container re-created
-rm -f '/run/.rte_config'
+# update command with vlan & bond options
+if [[ -n "$vlan_data" ]] ; then
+    vlan_id=$(echo "$vlan_data" | cut -d ' ' -f 1)
+    vlan_parent=$(echo "$vlan_data" | cut -d ' ' -f 2)
+    cmd+=" --vlan_tci ${vlan_id} --vlan_fwd_intf_name ${vlan_parent}"
+fi
+if [[ -n "$bond_data" ]] ; then
+    mode=$(echo "$bond_data" | cut -d ' ' -f 1)
+    policy=$(echo "$bond_data" | cut -d ' ' -f 2)
+    numa=$(echo "$bond_data" | cut -d ' ' -f 5)
+    # use list of slaves pci for next check of bind
+    pci_address=$(echo "$bond_data" | cut -d ' ' -f 4)
+    cmd+=" --vdev eth_bond_${phys_int},mode=${mode},xmit_policy=${policy},socket_id=${numa},mac=$phys_int_mac"
+    for s in ${pci_address//,/ } ; do
+        cmd+=",slave=${s}"
+    done
+fi
 
-# set maximum socket buffer size to (max hold flows entries * 9160 bytes)
-set_ctl net.core.wmem_max 9160000
+# ensure devices are bind to dpdk driver
+for pci in ${pci_address//,/ } ; do
+    wait_device_for_driver $DPDK_UIO_DRIVER $pci
+done
 
-echo "INFO: exec '$real_cmd'"
-exec $real_cmd
+echo "INFO: exec '$cmd'"
+exec $cmd
