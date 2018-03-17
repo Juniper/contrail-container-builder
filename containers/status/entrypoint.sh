9026dd0a46e683be517cc6ee2fed1635123d9d37
virtualhops <amonge@juniper.net>
2018-03-17 08:45:36 -0700

    container-status should use the container matching the installed Contrail version
    Change-Id: I6c6c83acb60edcefb70ee052fa4f24be2f57e26c
    Closes-Bug: 1756404

--- containers/status/entrypoint.sh
+++ containers/status/entrypoint.sh
@@ -7,7 +7,7 @@ fi
 
 cat > /usr/bin/contrail-status << EOM
 #!/bin/bash
-docker run -it --rm --name contrail-status -v /etc/contrail:/etc/contrail -v /usr/bin:/usr/bin -v /var/run/docker.sock:/var/run/docker.sock --pid host --net host --privileged contrail-status:latest
+docker run -it --rm --name contrail-status -v /etc/contrail:/etc/contrail -v /usr/bin:/usr/bin -v /var/run/docker.sock:/var/run/docker.sock --pid host --net host --privileged ${CONTRAIL_REPOSITORY}/contrail-status:${CONTRAIL_CONTAINER_TAG}
 EOM
 
 chmod +x /usr/bin/contrail-status
