050413a7f5e83e4faea375a87598d4249cfaaf27
virtualhops <amonge@juniper.net>
2018-03-19 07:38:54 -0700

    contrail-status command should consider the Contrail repo and version of the contrail-status container
    Change-Id: I10a1d8327d641149d6c112da29f450226d400d22
    Closes-Bug: 1756537

--- containers/status/entrypoint.sh
+++ containers/status/entrypoint.sh
@@ -5,9 +5,10 @@ if [ -f /usr/bin/contrail-status ]; then
    return $?
 fi
 
+source /version
 cat > /usr/bin/contrail-status << EOM
 #!/bin/bash
-docker run -it --rm --name contrail-status -v /etc/contrail:/etc/contrail -v /usr/bin:/usr/bin -v /var/run/docker.sock:/var/run/docker.sock --pid host --net host --privileged contrail-status:latest
+docker run -it --rm --name contrail-status -v /etc/contrail:/etc/contrail -v /usr/bin:/usr/bin -v /var/run/docker.sock:/var/run/docker.sock --pid host --net host --privileged ${CONTAINER_REGISTRY}/contrail-status:${CONTRAIL_VERSION}
 EOM
 
 chmod +x /usr/bin/contrail-status
