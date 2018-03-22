e418ff8bda0fb2b427e2d4d2fb9bab796b01cd8d
dineshb-jnpr <dineshb@juniper.net>
2018-03-22 15:22:36 -0700

    Contrail-k8s nested mode provisioning.
    These changes are to add support for provisioning of Contrail-k8s
    components in "nested" mode, in the microservice provision model.
    In nested mode, Contrail-kubernetes cluster is installed as overlay
    on an existing Contrail-openstack cluster.
    
    Change-Id: I077e55e65411361787bd7742ee72a08c652dff40
    Closes-Bug: #1756416

--- containers/kubernetes/kube-manager/entrypoint.sh
+++ containers/kubernetes/kube-manager/entrypoint.sh
@@ -17,14 +17,15 @@ token=$K8S_TOKEN
 log_file=$LOG_DIR/contrail-kube-manager.log
 log_level=$LOG_LEVEL
 log_local=$LOG_LOCAL
+nested_mode=${KUBEMANAGER_NESTED_MODE:-"0"}
 
 [KUBERNETES]
 kubernetes_api_server=${KUBERNETES_API_SERVER:-${DEFAULT_LOCAL_IP}}
 kubernetes_api_port=${KUBERNETES_API_PORT:-8080}
 kubernetes_api_secure_port=${KUBERNETES_API_SECURE_PORT:-6443}
 cluster_name=${KUBERNETES_CLUSTER_NAME:-"k8s"}
-cluster_project=${KUBERNETES_CLUSTER_PROJECT:-"{'domain': 'default-domain', 'project': 'default'}"}
-;cluster_network=${KUBERNETES_CLUSTER_NETWORK:-"{}"}
+cluster_project=${KUBERNETES_CLUSTER_PROJECT:-"{}"}
+cluster_network=${KUBERNETES_CLUSTER_NETWORK:-"{}"}
 pod_subnets=${KUBERNETES_POD_SUBNETS:-"10.32.0.0/12"}
 ip_fabric_subnets=${KUBERNETES_IP_FABRIC_SUBNETS:-"10.64.0.0/12"}
 service_subnets=${KUBERNETES_SERVICE_SUBNETS:-"10.96.0.0/12"}
@@ -37,6 +38,7 @@ vnc_endpoint_ip=$CONFIG_NODES
 vnc_endpoint_port=$CONFIG_API_PORT
 
 rabbit_server=$RABBITMQ_SERVERS
+rabbit_port=$RABBITMQ_NODE_PORT
 $rabbit_config
 $kombu_ssl_config
 
@@ -44,6 +46,16 @@ cassandra_server_list=$cassandra_server_list
 collectors=$COLLECTOR_SERVERS
 zk_server_ip=$ZOOKEEPER_SERVERS
 EOM
+
+if [[ $AUTH_MODE == "keystone" ]]; then
+    cat >> /etc/contrail/contrail-kubernetes.conf << EOM
+[AUTH]
+auth_user=${KEYSTONE_AUTH_ADMIN_USER:-''}
+auth_password=${KEYSTONE_AUTH_ADMIN_PASSWORD:-''}
+auth_tenant=${KEYSTONE_AUTH_ADMIN_TENANT:-''}
+auth_token_url=$KEYSTONE_AUTH_PROTO://${KEYSTONE_AUTH_HOST}:${KEYSTONE_AUTH_ADMIN_PORT}${KEYSTONE_AUTH_URL_TOKENS}
+EOM
+fi
 
 add_ini_params_from_env KUBERNETES /etc/contrail/contrail-kubernetes.conf
 
