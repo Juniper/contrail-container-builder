ef63dc8f50716971d434f05f97219b03344002e4
Andrey Pavlov <andrey.mp@gmail.com>
2018-03-23 15:55:43 +0300

    fix using ARG in Dockerfile
    Change-Id: Iba8e28f0d8d8dc7cb033ab9817c68862da59b42c
    Closes-Bug: #1758325

--- containers/status/entrypoint.sh
+++ containers/status/entrypoint.sh
@@ -2,12 +2,14 @@
 
 if [ -f /usr/bin/contrail-status ]; then
    exec "$@"
-   return $?
+   exit
 fi
 
+my_id=`awk -F'[:/]' '(($4 == "docker") && (lastId != $NF)) { lastId = $NF; print $NF; }' /proc/self/cgroup`
+my_image=`python -c "import docker; client = docker.from_env(); print(str(client.inspect_container('$my_id')['Config']['Image']))"`
 cat > /usr/bin/contrail-status << EOM
 #!/bin/bash
-docker run -it --rm --name contrail-status -v /etc/contrail:/etc/contrail -v /usr/bin:/usr/bin -v /var/run/docker.sock:/var/run/docker.sock --pid host --net host --privileged ${CONTAINER_REGISTRY}/contrail-status:${CONTRAIL_VERSION}
+docker run -it --rm --name contrail-status -v /etc/contrail:/etc/contrail -v /usr/bin:/usr/bin -v /var/run/docker.sock:/var/run/docker.sock --pid host --net host --privileged $my_image}
 EOM
 
 chmod +x /usr/bin/contrail-status
