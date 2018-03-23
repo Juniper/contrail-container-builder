71a65ac8acd417c8e510577da8467de543176078
alexey-mr <alexey.morlang@gmail.com>
2018-03-23 14:20:43 +0300

    Rework to avoid net-watchdog container. part1.
    Change-Id: Ibc4ca9ddcf86f7af3087a854723570fdbbfea586
    Partial-Bug: #1756961

--- containers/vrouter/kernel-init/entrypoint.sh
+++ containers/vrouter/kernel-init/entrypoint.sh
@@ -3,14 +3,9 @@
 # these next folders must be mounted to compile vrouter.ko in ubuntu: /usr/src /lib/modules
 
 source /common.sh
-# copy ifup-vhost and vif util
-if [[ ! -f /etc/sysconfig/network-scripts/ifup-vhost && -d /etc/sysconfig/network-scripts ]]; then
-  /bin/cp -f /ifup-vhost /etc/sysconfig/network-scripts
-  chmod +x /etc/sysconfig/network-scripts/ifup-vhost
-fi
-if [[ ! -f /host/bin/vif ]]; then
-  /bin/cp -f /bin/vif /host/bin/vif
-fi
+source /agent-functions.sh
+
+copy_agent_tools_to_host
 
 # Load kernel module
 if lsmod | grep -q vrouter; then
@@ -18,20 +13,15 @@ if lsmod | grep -q vrouter; then
   # TODO: handle upgrade
 else
   kver=`uname -r | awk -F "-" '{print $1}'`
-  echo "INFO: Load kernel module for kver=$kver"
-  modfile=`ls -1rt /opt/contrail/vrouter-kernel-modules/$kver-*/vrouter.ko | tail -1`
-  for k_dir in `ls -d /lib/modules/*`
-  do
+  echo "INFO: Load kernel module for kver=${kver}"
+  modfile=`ls -1rt /opt/contrail/vrouter-kernel-modules/${kver}-*/vrouter.ko | tail -1`
+  for k_dir in `ls -d /lib/modules/*` ; do
     mkdir -p ${k_dir}/kernel/net/vrouter
-    cp ${modfile} ${k_dir}/kernel/net/vrouter
+    cp -f ${modfile} ${k_dir}/kernel/net/vrouter
   done
   depmod -a
-  echo "INFO: Modprobing vrouter $modfile"
   free -h && sync && echo 2 >/proc/sys/vm/drop_caches && free -h
-  if ! insmod $modfile ; then
-    echo "ERROR: Failed to insert vrouter kernel module"
-    exit 1
-  fi
+  load_kernel_module vrouter
 fi
 
 exec $@
--- containers/vrouter/net-watchdog/entrypoint.sh
+++ containers/vrouter/net-watchdog/entrypoint.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
-source /common.sh
-source /agent-functions.sh
-
-TRACK_VHOST0=${TRACK_VHOST0:-'true'}
-TRACK_VHOST0_PAUSE=${TRACK_VHOST0_PAUSE:-5}
-
+# TODO: empty implemenetation,
+# this continaer is to deleted at all after removing references to it
+# from other repos
 while (true) ; do
-
-    echo "INFO: ip address show:"
-    ip address show
-
-    init_vhost0
-
-    if [[ "$TRACK_VHOST0" != 'true' ]] ; then
-        echo "INFO: TRACK_VHOST0 is $TRACK_VHOST0. Stop stracking."
-        break
-    fi
-    echo "INFO: wait for $TRACK_VHOST0_PAUSE seconds..."
-    sleep $TRACK_VHOST0_PAUSE
+    sleep 5
 done
 
 exec $@
