afdb315786f9679aa675d0c944bdfa4aa2998d84
Andrey Pavlov <andrey.mp@gmail.com>
2018-03-26 19:19:19 +0300

    add contrail-status to k8s manifests
    Change-Id: I1f73d1220594ae093046499d615be3b324c4592f
    Partial-Bug: #1744439

--- containers/status/entrypoint.sh
+++ containers/status/entrypoint.sh
@@ -5,6 +5,7 @@ if [ -f /host/usr/bin/contrail-status ]; then
    exit
 fi
 
+set -x
 my_id=`grep -E "/docker.*[0-9a-fA-F]{8,}" /proc/self/cgroup | tail -1 | grep -oE '[0-9a-fA-F]{8,}'`
 my_image=`python -c "import docker; client = docker.from_env(); print(str(client.inspect_container('$my_id')['Config']['Image']))"`
 cat > /host/usr/bin/contrail-status << EOM
