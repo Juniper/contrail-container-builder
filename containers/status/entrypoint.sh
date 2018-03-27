767155314b57cb234356bd8fc2893d10b417d641
Andrey Pavlov <andrey.mp@gmail.com>
2018-03-27 23:34:34 +0300

    handle one more type of cgroup file inside container
    k8s containers have another format of cgroup file:
    1:name=systemd:/system.slice/docker-85a58dea0e489d6fdfe6c23bcf581d73aeae33563ac75f4cdc5f46cb08912866.scope
    or
    7:memory:/kubepods/besteffort/pod7278f85057e8bf5cb81c9f96d3b25320/a224b95180fe6c8f379e934a56dc759674b7feb0eebeafd52785df38602fd0d4
    or
    2:memory:/kubepods.slice/kubepods-besteffort.slice/kubepods-besteffort-podb743d1b2_328a_11e8_bc33_525400648a8b.slice/docker-3dd331de8f42e8633e799af7b6f8e369f3729fa27a60827887094728d80d7366.scope
    
    supported format is just:
    2:cpuset:/docker/7be92808767a667f35c8505cbf40d14e931ef6db5b0210329cf193b15ba9d605
    
    Change-Id: Ib09658d2ad0ecef0d8b5211aa9bf6a85a826068a
    Partial-Bug: #1744439

--- containers/status/entrypoint.sh
+++ containers/status/entrypoint.sh
@@ -5,13 +5,26 @@ if [ -f /host/usr/bin/contrail-status ]; then
    exit
 fi
 
-my_id=`awk -F'[:/]' '(($4 == "docker") && (lastId != $NF)) { lastId = $NF; print $NF; }' /proc/self/cgroup`
-my_image=`python -c "import docker; client = docker.from_env(); print(str(client.inspect_container('$my_id')['Config']['Image']))"`
-cat > /host/usr/bin/contrail-status << EOM
+echo "This script takes environment variable IMAGE to create /usr/bin/contrail-status file."
+echo "If this variable is empty or absent then script tries to detect it."
+echo "But there is no exact method to do it - if you want to be sure"
+echo "then please pass IMAGE from outside by addind '--env IMAGE=' with IMAGE_ID"
+echo "to 'docker run' string for this container."
+my_image="${IMAGE}"
+if [[ -z "$my_image" ]]; then
+  my_id=`grep ':.*memory.*:' /proc/self/cgroup | head -1 | grep -oE "[0-9a-fA-F]{64}"`
+  if [[ -n "$my_id" ]]; then
+    my_image=`python -c "import docker; client = docker.from_env(); print(str(client.inspect_container('$my_id')['Config']['Image']))" 2>/dev/null`
+  fi
+fi
+if [[ -z "$my_image" ]]; then
+  echo "Self docker image couldn't be evaluated. Please provide IMAGE variable from outside to create /usr/bin/contrail-status file."
+else
+  cat > /host/usr/bin/contrail-status << EOM
 #!/bin/bash -e
 docker run --rm --name contrail-status -v /usr/bin:/host/usr/bin -v /var/run/docker.sock:/var/run/docker.sock --pid host --net host --privileged $my_image
 EOM
-
-chmod +x /host/usr/bin/contrail-status
+  chmod +x /host/usr/bin/contrail-status
+fi
 
 exec "$@"
