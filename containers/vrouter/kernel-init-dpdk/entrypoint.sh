30c676f3003d3c7d6a34557152660dc63c050e59
alexey-mr <alexey.morlang@gmail.com>
2018-04-08 21:18:41 +0300

    Fixed case with reboot.
    Init containers are not started at reboot,
    since they are to init pod which is actually already
    created.
    
    Depends-On: I79ec501725128aeff4ae2c7781b2fb3a9eb4e682
    Change-Id: I3504c860b90bcd7f7c6a23b7fdad91f2e83f5f76
    Closes-Bug: #1753567

--- containers/vrouter/kernel-init-dpdk/entrypoint.sh
+++ containers/vrouter/kernel-init-dpdk/entrypoint.sh
@@ -5,25 +5,4 @@ source /agent-functions.sh
 
 copy_agent_tools_to_host
 
-HUGE_PAGES_DIR=${HUGE_PAGES_DIR:-'/dev/hugepages'}
-ensure_hugepages $HUGE_PAGES_DIR
-
-set_ctl vm.nr_hugepages ${HUGE_PAGES}
-set_ctl vm.max_map_count 128960
-set_ctl net.ipv4.tcp_keepalive_time 5
-set_ctl net.ipv4.tcp_keepalive_probes 5
-set_ctl net.ipv4.tcp_keepalive_intvl 1
-
-load_kernel_module uio
-load_kernel_module "$DPDK_UIO_DRIVER"
-# multiple kthreads for port monitoring
-if ! load_kernel_module rte_kni kthread_mode=multiple ; then
-  echo "WARNING: rte_ini kernel module is unavailable. Please install/insert it for Ubuntu 14.04 manually."
-fi
-
-if ! prepare_phys_int_dpdk ; then
-  echo "FATAL: failed to initialize data for DPDK mode... exiting"
-  exit -1
-fi
-
 exec $@
