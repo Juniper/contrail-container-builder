6d3284c9aa89b555fd76312aa50597390d9f5549
Jackjvs <vsjagan@juniper.net>
2018-04-23 15:00:17 -0700

    Remove building and deployments of the following containers:     contrail-analytics-topology and contrail-analytics-snmp-collector Internal decision is to not bundle topology and snmp-collector with contrail-analytics. To go with the decision, deployment is being disabled fist. Reviews to disable the building of these containers will also be posted.
    Change-Id: I2a34ca038fe0dafaac4031c3881234819483e301
    Partial-Bug: #1765251

--- containers/status/contrail-status.py
+++ containers/status/contrail-status.py
@@ -52,8 +52,6 @@ CONTRAIL_SERVICES_TO_SANDESH_SVC = {
         'collector': 'contrail-collector',
         'query-engine': 'contrail-query-engine',
         'alarm-gen': 'contrail-alarm-gen',
-        'snmp-collector': 'contrail-snmp-collector',
-        'topology': 'contrail-topology',
     },
     'kubernetes': {
         'kube-manager': 'contrail-kube-manager',
