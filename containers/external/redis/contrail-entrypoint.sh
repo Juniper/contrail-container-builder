1ea53c07204a12bdc608ce1a28828270f36613d8
zcui <zcui@juniper.net>
2018-07-26 15:24:51 -0700

    Default Redis config is being used instead of the customize redis config
    Legecy contrail will change default configuration of redis, but these
    changes has gone in recent version
    
    Change-Id: I1289f30e6b22fbc1b1885adbfd3ba81f33586306
    Partial-bug: 1780354

---
+++ containers/external/redis/contrail-entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+
+REDIS_PORT=${REDIS_PORT:-6379}
+
+if [[ -n $REDIS_NODES ]]; then
+  for i in {1..10} ; do
+    ord=1
+    my_ord=0
+    IFS=',' read -ra srv_list <<< "$REDIS_NODES"
+    local_ips=",$(cat "/proc/net/fib_trie" | awk '/32 host/ { print f } {f=$2}' | tr '\n' ','),"
+    redis_servers=''
+    for srv in "${srv_list[@]}"; do
+      if [[ "$local_ips" =~ ",$srv," ]] ; then
+         redis_node_ip=${srv}
+        echo "INFO: found '$srv' in local IPs '$local_ips'"
+        my_ord=$ord
+      fi
+      ord=$((ord+1))
+    done
+    if (( $my_ord > 0 && $my_ord <= "${#srv_list[@]}" )); then
+      break
+    fi
+    sleep 1
+  done
+fi
+
+REDIS_LISTEN_ADDRESS=${REDIS_LISTEN_ADDRESS:-${redis_node_ip}}
+
+if [[ -n "$REDIS_LISTEN_ADDRESS" ]] ; then
+  redis_opts+="--bind \"$REDIS_LISTEN_ADDRESS 127.0.0.1\""
+fi
+if [[ -n "$REDIS_PORT" ]] ; then
+  redis_opts+="--port $REDIS_PORT"
+fi
+redis_opts+=" --lua-time-limit 15000"
+redis_opts+=" --dbfilename ''"
+if [[ -n "$REDIS_SERVER_PASSWORD" ]] ; then
+  redis_opts+=" --requirepass $REDIS_SERVER_PASSWORD"
+fi
+exec /docker-entrypoint.sh $@
