56f8a881e9f420feb912fc653587bdc30a67a8c1
Damian Szeluga <dszeluga@juniper.net>
2018-07-27 08:47:47 +0200

    Adding pycassa, gevent and pysandesh to copied modules
    Change-Id: I2d0be9cbc09ab490682781486d5598b047d36eb1
    Closes-Bug: #1783947
    Depends-On: I56de48b7ee8d8fe910488bb500258adc46a060fc

--- containers/openstack/heat-init/entrypoint.sh
+++ containers/openstack/heat-init/entrypoint.sh
@@ -4,10 +4,19 @@ set -ex
 
 # linux distro here always centos for now
 src_path='/usr/lib/python2.7/site-packages'
+src64_path='/usr/lib64/python2.7/site-packages'
 
 mkdir -p /opt/plugin/site-packages
-for module in contrail_heat vnc_api cfgm_common ; do
-  for item in `ls -d $src_path/${module}*` ; do
-    cp -r $item /opt/plugin/site-packages/
-  done
+for module in contrail_heat vnc_api cfgm_common pycassa pysandesh gevent ; do
+  if [[ -d ${src_path}/${module} ]]; then
+    for item in `ls -d $src_path/${module}*` ; do
+      cp -r ${item} /opt/plugin/site-packages/
+    done
+  else
+    if [[ -d ${src64_path}/${module} ]]; then
+      for item in `ls -d ${src64_path}/${module}*` ; do
+        cp -r ${item} /opt/plugin/site-packages/
+      done
+    fi
+  fi
 done
