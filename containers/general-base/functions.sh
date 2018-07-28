4b42bcfb960d32343f1c674cff5b481969836729
Sanju Abraham <asanju@gmail.com>
2018-07-27 21:27:30 -0700

    This fix address the proper configuration of the ip address with mask in contrail-vrouter-agent.conf i for Google Cloud
    Change-Id: I9eb73993196d860b0da963cf1dccace3ebe58764
    Closes-Bug: #1783570

--- containers/general-base/functions.sh
+++ containers/general-base/functions.sh
@@ -146,3 +146,23 @@ function get_ip_for_vrouter_from_control() {
   local iface=$(get_iface_for_vrouter_from_control)
   get_listen_ip_for_nic $iface
 }
+
+function mask2cidr() {
+  nbits=0
+  IFS=.
+  for dec in $1 ; do
+        case $dec in
+            255) let nbits+=8;;
+            254) let nbits+=7;;
+            252) let nbits+=6;;
+            248) let nbits+=5;;
+            240) let nbits+=4;;
+            224) let nbits+=3;;
+            192) let nbits+=2;;
+            128) let nbits+=1;;
+            0);;
+            *) echo "Error: $dec is not recognised"; exit 1
+        esac
+  done
+  echo "$nbits"
+}
