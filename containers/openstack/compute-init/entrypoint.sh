b4d5dd38e81633c9d763e8a70eed7dfb86d5db0c
Andrey Pavlov <andrey.mp@gmail.com>
2018-08-07 15:14:56 +0300

    Add copying of new part of code for vrouter into nova-compute
    Change-Id: Ifb9c568f9b241d0f1f5ccd03c031f22a2609940c
    Partial-Bug: #1773981

--- containers/openstack/compute-init/entrypoint.sh
+++ containers/openstack/compute-init/entrypoint.sh
@@ -8,7 +8,7 @@ cp /opt/contrail/bin/vrouter-port-control /opt/plugin/bin/
 src_path='/usr/lib/python2.7/site-packages'
 
 mkdir -p /opt/plugin/site-packages
-for module in vif_plug_vrouter nova_contrail_vif ; do
+for module in vif_plug_vrouter vif_plug_contrail_vrouter nova_contrail_vif ; do
   for item in `ls -d $src_path/${module}*` ; do
     cp -r $item /opt/plugin/site-packages/
   done
