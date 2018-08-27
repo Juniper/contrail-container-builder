237ff76d3f82d381b01c5ec9ee6bbe5db3e6f744
alexey-mr <alexey.morlang@gmail.com>
2018-08-27 14:27:10 +0300

    Ability to pass vrouter kernel module opts
    New env variable VROUTER_MODULE_OPTIONS
    allows to pass vrouter kernel module
    paramters into modprobe call.
    
    Change-Id: Ie0ca34b1dedb4193e14faf63f63b2a0d97be8de6
    Partial-Bug: #1780954

--- containers/vrouter/kernel-init/entrypoint.sh
+++ containers/vrouter/kernel-init/entrypoint.sh
@@ -7,19 +7,13 @@ source /agent-functions.sh
 
 copy_agent_tools_to_host
 
-# Load kernel module
-if lsmod | grep -q vrouter; then
-  echo "INFO: vrouter.ko already loaded in the system"
-  # TODO: handle upgrade
-else
-  kver=`uname -r | awk -F "-" '{print $1}'`
-  echo "INFO: Load kernel module for kver=${kver}"
-  modfile=`ls -1rt /opt/contrail/vrouter-kernel-modules/${kver}-*/vrouter.ko | tail -1`
-  for k_dir in `ls -d /lib/modules/*` ; do
-    mkdir -p ${k_dir}/kernel/net/vrouter
-    cp -f ${modfile} ${k_dir}/kernel/net/vrouter
-  done
-  depmod -a
-fi
+kver=`uname -r | awk -F "-" '{print $1}'`
+echo "INFO: copy kernel module for kver=${kver} to host"
+modfile=`ls -1rt /opt/contrail/vrouter-kernel-modules/${kver}-*/vrouter.ko | tail -1`
+for k_dir in `ls -d /lib/modules/*` ; do
+  mkdir -p ${k_dir}/kernel/net/vrouter
+  cp -f ${modfile} ${k_dir}/kernel/net/vrouter
+done
+depmod -a
 
 exec $@
