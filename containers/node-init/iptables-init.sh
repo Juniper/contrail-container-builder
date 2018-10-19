630ac62b046c422ec653a9110b5fc372c56fde6b
dineshb-jnpr <dineshb@juniper.net>
2018-10-19 13:51:26 -0700

    Add iptable config support to node-init container
    This commit adds the ability to configure iptable rules on a node
    through the node-init container.
    
    This scripts works off the following environment variables to see
    if iptable configuration is required. If required, then it deciphers
    the table, chain and port info using addition env variables.
    
    CONFIGURE_IPTABLES  -> Set to true/yes/enabled if iptable configuration is required.
                           Default value is 'false'
    IPTABLES_OPEN_PORTS -> Ports to be opened up in iptables, delimited by ','.
                           Multiple ports or port-ranges may be specified.
                           Default value is ''
    IPTABLES_CHAIN      -> Name of the chain in iptables.
                           This chain, if specified, MUST exist.
                           Default value is "INPUT"
    IPTABLES_TABLE      -> Name of the table in iptables.
                           This table, if specified, MUST exist.
                           Default value is "filter"
    
    With this change, ports for a kubernetes pod can be opened using the node-init
    container as pod's pre-init container.
    
    This negates the need to know/use host's firewall mechanism(i.e firewalld, ipables etc).
    
    Change-Id: Iff634d3596b9d4bc07385069cadd53a7363abcf4
    Partial-Bug: #1795750

---
+++ containers/node-init/iptables-init.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+source /functions.sh
+
+#
+# Script to configure iptables rules on a node.
+#
+# This script works off the following environment variables:
+#
+# IPTABLES_OPEN_PORTS -> Ports to be opened up in iptables, delimited by ','.
+#                        Multiple ports or port-ranges may be specified.
+#                        Default value is ''
+# IPTABLES_CHAIN      -> Name of the chain in iptables.
+#                        This chain, if specified, MUST exist.
+#                        Default value is "INPUT"
+# IPTABLES_TABLE      -> Name of the table in iptables.
+#                        This table, if specified, MUST exist.
+#                        Default value is "filter"
+# CONFIGURE_IPTABLES  -> Set to true/yes/enabled if iptable configuration is
+#                        being required.
+#                        Default value is 'false'
+#
+# Script returns the following return value:
+#
+# 0 - Success
+# 1 - All failures.
+#
+
+script_name=`basename "$0"`
+CONFIGURE=${CONFIGURE_IPTABLES:-'false'}
+
+function configure_iptable_ports_allow_rules() {
+
+  IPT="iptables"
+  IPT_TABLE_NAME=${IPTABLES_TABLE:-'filter'}
+  IPT_CHAIN_NAME=${IPTABLES_CHAIN:-'INPUT'}
+  local dst_port=${IPTABLES_OPEN_PORTS:-''}
+
+  echo "$script_name: Configuring iptable rules to open port/s: [$dst_port] in chain: [$IPT_CHAIN_NAME] in Table: [$IPT_TABLE_NAME]"
+
+  # Check if the requested chain exists.
+
+  echo "$script_name: Validate that chain [$IPT_CHAIN_NAME] / table [$IPT_TABLE_NAME] exists."
+  iptables --list $IPT_CHAIN_NAME -t $IPT_TABLE_NAME -n
+  if [ $? -ne 0 ]; then
+    echo "$script_name: Chain [$IPT_CHAIN_NAME] does not exist in table [$IPT_TABLE_NAME]"
+    echo "$script_name: Exiting with failure."
+    exit 1
+  fi
+
+  # Deduce the list of ports to be opened/allowed.
+  local dst_port_list=''
+  IFS=',' read -ra dst_port_list <<< "${dst_port}"
+
+  for port in "${dst_port_list[@]}"; do
+
+    # Deduce if any rule for this port already exists in the requested chain..
+    iptables --list $IPT_CHAIN_NAME -t $IPT_TABLE_NAME -n | grep -q $port
+    local rule_exists=$?
+
+    if  [ $rule_exists  -ne 0 ] ; then
+      # No rule targeting this port exists in the requested chain.
+      echo "$script_name: Adding rule: $IPT -t $IPT_TABLE_NAME -I $IPT_CHAIN_NAME 1 -w 5 -W 100000 -p tcp --dport $port -j ACCEPT"
+      $IPT -t $IPT_TABLE_NAME -I $IPT_CHAIN_NAME 1 -w 5 -W 100000 -p tcp --dport $port -j ACCEPT
+      local add_result=$?
+
+      if [ $add_result -ne 0 ]; then
+        echo "$script_name: ERROR Adding rule: $IPT -t $IPT_TABLE_NAME -I $IPT_CHAIN_NAME 1 -w 5 -W 100000 -p tcp --dport $port -j ACCEPT"
+        echo "$script_name: Exiting with failure."
+        exit 1
+      fi
+    else
+      echo "$script_name: A rule for port [$port] exists in chain [$IPT_CHAIN_NAME] table [$IPT_TABLE_NAME]. Skipping config for this port."
+    fi
+  done
+
+}
+
+# Configure iptable rules if requested.
+if is_enabled $CONFIGURE; then
+  configure_iptable_ports_allow_rules
+fi
+
+exit
