c40a3ce8b40cb3b76301886100dc6c8f9d25eb7b
Andrey Pavlov <andrey.mp@gmail.com>
2018-11-10 17:20:56 +0300

    remove installing python-neutron-lbaas for neutron-init container
    Change-Id: I001b8a6fdb8fff5249177b9cc6fb929f35e35bac
    Partial-Bug: #1802651

--- containers/openstack/neutron-init/entrypoint.sh
+++ containers/openstack/neutron-init/entrypoint.sh
@@ -4,7 +4,7 @@
 src_path='/usr/lib/python2.7/site-packages'
 
 mkdir -p /opt/plugin/site-packages
-for module in neutron_plugin_contrail vnc_api cfgm_common neutron_lbaas ; do
+for module in neutron_plugin_contrail vnc_api cfgm_common ; do
   for item in `ls -d $src_path/${module}*` ; do
     cp -r $item /opt/plugin/site-packages/
   done
