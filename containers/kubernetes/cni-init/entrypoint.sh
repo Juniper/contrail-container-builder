67e1f351220a1717d5b07a14b151fb9f31c7c108
Venkatraman Venkatapathy <vvenkatapath@juniper.net>
2018-11-13 11:30:22 -0800

    Support for Mesos CNI container creation
    Extending kuberentes cni container to support mesos
    Partial-bug: #1799320
    
    Change-Id: I178ccd2a90248d3d8ad0df287441bb4a7ab4dcfe

--- containers/kubernetes/cni-init/entrypoint.sh
+++ containers/kubernetes/cni-init/entrypoint.sh
@@ -4,6 +4,9 @@ source /common.sh
 
 VROUTER_PORT=${VROUTER_PORT:-9091}
 KUBEMANAGER_NESTED_MODE=${KUBEMANAGER_NESTED_MODE:-'0'}
+MESOS_IP=${MESOS_IP:-'localhost'}
+MESOS_PORT=${MESOS_PORT:-6991}
+CLUSTER_NAME=${CLUSTER_NAME:-'mesos'}
 run_command="$@"
 
 trap cleanup SIGHUP SIGINT SIGTERM
@@ -14,6 +17,45 @@ ulimit -d unlimited
 ulimit -v unlimited
 ulimit -n 4096
 
+if [ $CLOUD_ORCHESTRATOR == 'mesosphere' ]; then
+# In Mesos Environment
+mkdir -p /host/log_cni
+mkdir -p /host/mesos_etc_dcos_network/cni
+mkdir -p /host/opt_mesos_active_cni
+mkdir -p /var/lib/contrail/ports/vm
+
+cp /usr/bin/contrail-k8s-cni /host/opt_mesos_active_cni/contrail-cni-plugin
+chmod 0755 /host/opt_mesos_active_cni/contrail-cni-plugin
+
+tar -C /host/opt_mesos_active_cni -xzf /opt/cni-v0.3.0.tgz
+
+# Prepare config for Mesos CNI plugin
+# Note: Uses 127.0.0.1 as VROUTER_IP because it is always run on
+# the node with vrouter agent
+cat << EOM > /host/mesos_etc_dcos_network/cni/contrail-cni-plugin.conf
+{
+    "cniVersion": "0.3.1",
+    "contrail" : {
+        "vrouter-ip"    : "127.0.0.1",
+        "vrouter-port"  : $VROUTER_PORT,
+        "cluster-name"  : $CLUSTER_NAME,
+        "config-dir"    : "/var/lib/contrail/ports/vm",
+        "poll-timeout"  : 5,
+        "poll-retries"  : 15,
+        "log-file"      : "/var/log/contrail/cni/opencontrail.log",
+        "log-level"     : "debug",
+        "mesos-ip"      : $MESOS_IP,
+        "mesos-port"    : $MESOS_PORT,
+        "mode"          : "mesos"
+    },
+
+    "name": "contrail-mesos-cni",
+    "type": "contrail-mesos-cni"
+}
+EOM
+
+else
+# In Kubernetes environment
 mkdir -p /host/log_cni
 mkdir -p /host/etc_cni/net.d
 mkdir -p /host/opt_cni_bin
@@ -70,6 +112,7 @@ cat << EOM > /host/etc_cni/net.d/10-contrail.conf
        "log-file"          : "/var/log/contrail/cni/opencontrail.log",
        "log-level"         : "4"
    },
+
    "name": "contrail-k8s-cni",
    "type": "contrail-k8s-cni"
 }
@@ -80,6 +123,7 @@ EOM
 # as K8s daemon set. So just loop forever to not let the pod die.
 # This can be reworked once K8s supports "run once" daemonset scheme.
 run_command="tail -f /dev/null"
+fi
 fi
 
 exec $run_command
