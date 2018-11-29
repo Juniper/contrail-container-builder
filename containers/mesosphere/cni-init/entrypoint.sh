b0fe8df7cdfeefa3a23de824d9eab2b194f358c7
Venkatraman Venkatapathy <vvenkatapath@juniper.net>
2018-11-29 14:35:00 -0800

    Setting mesos ip and port values in cni conf file
    Populating the correct fields for mesos ip and port in cni conf
    file
    
    Change-Id: I2b723da2af84f14d32fb7c4aed177b9f8725b838
    Partial-bug: #1795295

--- containers/mesosphere/cni-init/entrypoint.sh
+++ containers/mesosphere/cni-init/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 source /common.sh
+source /functions.sh
 
+DEFAULT_IP_ADDRESS=$(get_default_ip)
+VROUTER_IP=${VROUTER_IP:-${DEFAULT_IP_ADDRESS}}
 VROUTER_PORT=${VROUTER_PORT:-9091}
-MESOS_IP=${MESOS_IP:-'localhost'}
+MESOS_IP=${MESOS_IP:-${DEFAULT_IP_ADDRESS}}
 MESOS_PORT=${MESOS_PORT:-6991}
 CLUSTER_NAME=${CLUSTER_NAME:-'mesos'}
 
@@ -37,11 +40,11 @@ cat << EOM > /host/opt_mesos_etc_dcos_network/cni/contrail-cni-plugin.conf
         "log-file"      : "/var/log/contrail/cni/opencontrail.log",
         "log-level"     : "4",
         "vrouter-port"  : $VROUTER_PORT,
-        "vrouter-ip"    : "127.0.0.1",
+        "vrouter-ip"    : "$VROUTER_IP",
         "mode"          : "mesos",
-        "cluster-name"  : $CLUSTER_NAME,
-        "mesos-ip"      : $MESOS_IP,
-        "mesos-port"    : $MESOS_PORT
+        "cluster-name"  : "$CLUSTER_NAME",
+        "mesos-ip"      : "$MESOS_IP",
+        "mesos-port"    : "$MESOS_PORT"
     },
 
     "name": "contrail-cni-plugin",
