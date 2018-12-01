2cae0e677c82e45e7827afb978c83eabe5bd797f
Ramprakash <rammohan@juniper.net>
2018-11-30 22:46:43 -0800

    Enable SSL/TLS for kafka
    TODO: Once this is merged, then the alarm-gen and collector entrypoints
    should be changed to remove the kafka_broker_list from the [DEFAULT]
    section of the conf file
    
    Partial-Bug: #1800926
    Depends-On: I254bb0896d69df3f148dccf80cb248b6cec506db
    Change-Id: Idda58a77099f78b0ee1ec070dae9e2669a89b2c3

--- containers/external/kafka/docker-entrypoint.sh
+++ containers/external/kafka/docker-entrypoint.sh
@@ -15,6 +15,7 @@ ANALYTICSDB_NODES=${ANALYTICSDB_NODES:-${ANALYTICS_NODES}}
 KAFKA_NODES=${KAFKA_NODES:-${ANALYTICSDB_NODES}}
 ZOOKEEPER_ANALYTICS_NODES=${ZOOKEEPER_ANALYTICS_NODES:-${ANALYTICSDB_NODES}}
 ZOOKEEPER_ANALYTICS_PORT=${ZOOKEEPER_ANALYTICS_PORT:-2182}
+KAFKA_SSL_ENABLE=${KAFKA_SSL_ENABLE:-${SSL_ENABLE:-False}}
 
 : ${KAFKA_LISTEN_ADDRESS='auto'}
 my_index=1
@@ -71,10 +72,48 @@ KAFKA_log_cleaner_threads=${KAFKA_log_cleaner_threads:-2}
 KAFKA_log_cleaner_dedupe_buffer_size=${KAFKA_log_cleaner_dedupe_buffer_size:-250000000}
 KAFKA_log_cleaner_enable=${KAFKA_log_cleaner_enable:-true}
 KAFKA_delete_topic_enable=${KAFKA_delete_topic_enable:-true}
+KAFKA_KEY_PASSWORD=${KAFKA_KEY_PASSWORD:-c0ntrail123}
+KAFKA_STORE_PASSWORD=${KAFKA_STORE_PASSWORD:-c0ntrail123}
 
 sed -i "s/^broker.id=.*$/broker.id=$KAFKA_BROKER_ID/g" ${CONFIG}
 sed -i "s/#port=.*$/port=$KAFKA_LISTEN_PORT/g" ${CONFIG}
-sed -i "s/#listeners=.*$/listeners=PLAINTEXT:\/\/$KAFKA_LISTEN_ADDRESS:$KAFKA_LISTEN_PORT/g" ${CONFIG}
+if [[ ${KAFKA_SSL_ENABLE} == "False" ]] ; then
+    sed -i "s/^listeners=.*$/listeners=PLAINTEXT:\/\/$KAFKA_LISTEN_ADDRESS:$KAFKA_LISTEN_PORT/g" ${CONFIG}
+else
+    export RANDFILE=${KAFKA_DIR}/.rnd
+    if [ -e ${KAFKA_DIR}/kafka.server.keystore.jks ] ; then
+      rm -f ${KAFKA_DIR}/kafka.server.keystore.jks
+    fi
+    if [ -e ${KAFKA_DIR}/kafka.server.truststore.jks ] ; then
+      rm -f ${KAFKA_DIR}/kafka.server.truststore.jks
+    fi
+    keytool -keystore ${KAFKA_DIR}/kafka.server.truststore.jks \
+            -keypass ${KAFKA_KEY_PASSWORD} -storepass ${KAFKA_STORE_PASSWORD} \
+            -noprompt \
+            -alias CARoot -import -file /etc/contrail/ssl/certs/ca-cert.pem
+    openssl pkcs12 -export -in /etc/contrail/ssl/certs/server.pem \
+            -inkey /etc/contrail/ssl/private/server-privkey.pem \
+            -chain -CAfile /etc/contrail/ssl/certs/ca-cert.pem \
+            -password pass:${KAFKA_KEY_PASSWORD} -name localhost -out TmpFile
+    keytool -importkeystore -deststorepass ${KAFKA_KEY_PASSWORD} \
+            -destkeystore ${KAFKA_DIR}/kafka.server.keystore.jks \
+            -srcstorepass ${KAFKA_STORE_PASSWORD} -srckeystore TmpFile \
+            -srcstoretype PKCS12 -alias localhost
+    keytool -keystore ${KAFKA_DIR}/kafka.server.keystore.jks \
+            -keypass ${KAFKA_KEY_PASSWORD} -storepass ${KAFKA_STORE_PASSWORD} \
+            -noprompt \
+            -alias CARoot -import -file /etc/contrail/ssl/certs/ca-cert.pem
+    echo "Created keystore"
+    sed -i "s/^listeners=.*$/listeners=SSL:\/\/$KAFKA_LISTEN_ADDRESS:$KAFKA_LISTEN_PORT/g" ${CONFIG}
+    (grep -q '^advertised.listeners' ${CONFIG} && sed -i "s|^advertised.listeners.*$|advertised.listeners=SSL:\/\/$KAFKA_LISTEN_ADDRESS:$KAFKA_LISTEN_PORT|" ${CONFIG}) || echo "advertised.listeners=SSL:\/\/$KAFKA_LISTEN_ADDRESS:$KAFKA_LISTEN_PORT" >> ${CONFIG}
+    (grep -q '^ssl.keystore.location' ${CONFIG} && sed -i "s|^ssl.keystore.location.*$|ssl.keystore.location=${KAFKA_DIR}/kafka.server.keystore.jks|" ${CONFIG}) || echo "ssl.keystore.location=${KAFKA_DIR}/kafka.server.keystore.jks" >> ${CONFIG}
+    (grep -q '^ssl.truststore.location' ${CONFIG} && sed -i "s|^ssl.truststore.location.*$|ssl.truststore.location=${KAFKA_DIR}/kafka.server.truststore.jks|" ${CONFIG})  || echo "ssl.truststore.location=${KAFKA_DIR}/kafka.server.truststore.jks" >> ${CONFIG}
+    (grep -q '^ssl.keystore.password' ${CONFIG} && sed -i "s|^ssl.keystore.password.*$|ssl.keystore.password=${KAFKA_STORE_PASSWORD}|" ${CONFIG}) || echo "ssl.keystore.password=${KAFKA_STORE_PASSWORD}" >> ${CONFIG}
+    (grep -q '^ssl.key.password' ${CONFIG} && sed -i "s|^ssl.key.password.*$|ssl.key.password=${KAFKA_KEY_PASSWORD}|" ${CONFIG}) || echo "ssl.key.password=${KAFKA_KEY_PASSWORD}" >> ${CONFIG}
+    (grep -q '^ssl.truststore.password' ${CONFIG} && sed -i "s|^ssl.truststore.password.*$|ssl.truststore.password=${KAFKA_STORE_PASSWORD}|" ${CONFIG}) || echo "ssl.truststore.password=${KAFKA_STORE_PASSWORD}" >> ${CONFIG}
+    (grep -q '^security.inter.broker.protocol' ${CONFIG} && sed -i 's|^security.inter.broker.protocol.*$|security.inter.broker.protocol=SSL|' ${CONFIG}) || echo "security.inter.broker.protocol=SSL" >> ${CONFIG}
+fi
+
 sed -i "s)^zookeeper.connect=.*$)zookeeper.connect=$zk_list)g" ${CONFIG}
 sed -i "s/#advertised.host.name=.*$/advertised.host.name=$my_ip/g" ${CONFIG}
 sed -i "s/^#log.retention.bytes=.*$/log.retention.bytes=$KAFKA_log_retention_bytes/g" ${CONFIG}
