fb97ecb3334d56b76c0624b48f50a3c5159ff154
Andrey Pavlov <andrey.mp@gmail.com>
2018-12-27 23:26:40 +0300

    remove copy-paste from external containers
    external containers didn't have access to common definitions and functions.
    solution is to copy common.sh and functions into external containers and use
    code from it to reduce copy-paste.
    
    Change-Id: Ia592aa3dfa86728f8f794bfbb59e56a307577181
    closes-jira-bug: JCB-218707

---
+++ containers/external/zookeeper/functions.sh
@@ -0,0 +1,1 @@
+../../general-base/functions.sh
--- containers/external/zookeeper/liveness.sh
+++ containers/external/zookeeper/liveness.sh
@@ -1,20 +1,8 @@
 #!/bin/bash -e
 
-IFS=',' read -ra srv_list <<< "$ZOOKEEPER_NODES"
-local_ips=",$(cat "/proc/net/fib_trie" | awk '/32 host/ { print f } {f=$2}' | tr '\n' ','),"
-for srv in "${srv_list[@]}"; do
-  if [[ "$local_ips" =~ ",$srv," ]] ; then
-    echo "INFO: found '$srv' in local IPs '$local_ips'"
-    my_ip=$srv
-    break
-  fi
-done
+source /common.sh
 
-if [ -z "$my_ip" ]; then
-  echo "ERROR: Cannot find self ips ('$local_ips') in Cassandra nodes ('$CASSANDRA_SEEDS')"
-  exit 1
-fi
-
+my_ip=$(get_listen_ip_for_node ZOOKEEPER)
 OK=$(echo ruok | nc $my_ip $ZOOKEEPER_PORT)
 if [ "$OK" == "imok" ]; then
     exit 0
