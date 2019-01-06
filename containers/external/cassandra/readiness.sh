4de67d564f3f3209b32032d68b3a86cd55f1cff2
Andrey Pavlov <andrey.mp@gmail.com>
2019-01-06 12:37:26 +0300

    remove obsolete code and move some code from general-base to specific containers
    Change-Id: I8327c41a5f596cd89c55981b93f82858170a8b07
    closes-jira-bug: JCB-218707

--- containers/external/cassandra/readiness.sh
+++ containers/external/cassandra/readiness.sh
@@ -4,7 +4,7 @@ source /common.sh
 
 my_ip=$(find_my_ip_and_order_for_node_list "$CASSANDRA_SEEDS" | cut -d ' ' -f 1)
 if [[ -z "$my_ip" ]] ; then
-  my_ip=$(get_default_ip)
+  my_ip=$DEFAULT_LOCAL_IP
 fi
 
 if ! nodetool status -p "$CASSANDRA_JMX_LOCAL_PORT" | grep -E "^UN\\s+$my_ip"; then
--- containers/vcenter/plugin/entrypoint.sh
+++ containers/vcenter/plugin/entrypoint.sh
@@ -2,6 +2,17 @@
 
 source /common.sh
 
+# TODO: implement using list of IP-s/Names in vcenter-plugin
+# to be able to pass a list of config-api server here.
+# and remove next block after.
+function get_vip_for_node() {
+  local ip=$(find_my_ip_and_order_for_node $1 | cut -d ' ' -f 1)
+  if [[ -z "$ip" ]] ; then
+    local server_typ=$1_NODES
+    ip=$(echo ${!server_typ} | cut -d',' -f 1)
+  fi
+  echo $ip
+}
 api_hostname=${CONFIG_API_VIP:-$(get_vip_for_node CONFIG)}
 
 cat > /etc/contrail/contrail-vcenter-plugin.conf << EOM
