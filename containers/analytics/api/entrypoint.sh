4a51f4d61e8e84efc5a9107b65c46a4b8aabce14
Biswajit Mandal <bmandal@juniper.net>
2019-01-07 07:00:43 +0000

    Do not populate kafka related config in collector config file if ENABLE_ANALYTICS_DATABASE_KAFKA is set as false or undefined. Call introspect call for contrail-status for kafka if ENABLE_ANALYTICS_DATABASE_KAFKA is set as True snmp-collector/topology if ENABLE_ANALYTICS_SNMP is set as True, alarm if ENABLE_ANALYTICS_ALARM is set to true
    Introduced new variable for cassandra/kafka db based on new roles.
    
    Config File changes:
    
    alarm-gen:
    {{zk_list}}: zookeeper list from configdb
    
    api:
    {{partitions}}: Only if Kafka enabled
    {{zk_list}}: zookeeper list from configdb
    {{redis_query_port}}: Only if Cassandra enabled
    
    collector:
    {{kafka_broker_list}}: Only if Kafka enabled
    {{zookeeper_server_list}}: zookeeper list from configdb
    {{[DATABASE]}}: Only of Cassandra enabled
    
    query-engine:
    No Change
    
    snmp-collector:
    {{zookeeper}}: zookeeper list from configdb
    topology:
    {{zookeeper}}: zookeeper list from configdb
    
    Conflicts:
        	containers/external/kafka/docker-entrypoint.sh
    
    Change-Id: I8eb42992351595c086b6b2e3eeac0ff9253de599
    Partial-Bug: #1784493

--- containers/analytics/api/entrypoint.sh
+++ containers/analytics/api/entrypoint.sh
@@ -13,7 +13,14 @@ http_server_port=${ANALYTICS_API_INTROSPECT_LISTEN_PORT:-$ANALYTICS_API_INTROSPE
 http_server_ip=$(get_introspect_listen_ip_for_node ANALYTICS)
 rest_api_port=${ANALYTICS_API_LISTEN_PORT:-$ANALYTICS_API_PORT}
 rest_api_ip=${ANALYTICS_API_LISTEN_IP:-$host_ip}
+EOM
+if [[ $ANALYTICS_ALARM_ENABLED == 'True' ]] ; then
+cat >> /etc/contrail/contrail-analytics-api.conf << EOM
 partitions=${ANALYTICS_UVE_PARTITIONS:-30}
+EOM
+fi
+
+cat >> /etc/contrail/contrail-analytics-api.conf << EOM
 aaa_mode=$AAA_MODE
 log_file=$LOG_DIR/contrail-analytics-api.log
 log_level=$LOG_LEVEL
@@ -22,12 +29,18 @@ log_local=$LOG_LOCAL
 # second. System logs are dropped if the sending rate is exceeded
 #sandesh_send_rate_limit =
 collectors=$COLLECTOR_SERVERS
-cassandra_server_list=$ANALYTICSDB_CQL_SERVERS
 api_server=$CONFIG_SERVERS
-zk_list=$ZOOKEEPER_ANALYTICS_SERVERS_SPACE_DELIM
+zk_list=$ZOOKEEPER_SERVERS_SPACE_DELIM
 
 [REDIS]
+EOM
+
+if [[ $ANALYTICSDB_ENABLED == 'True' ]] ; then
+cat >> /etc/contrail/contrail-analytics-api.conf << EOM
 redis_query_port=$REDIS_SERVER_PORT
+EOM
+fi
+cat >> /etc/contrail/contrail-analytics-api.conf << EOM
 redis_uve_list=$REDIS_SERVERS
 redis_password=$REDIS_SERVER_PASSWORD
 
