a27c3488e32b8ca81813c8c2e00c3aff60285205
Pawel Marchewka <pawel.marchewka@codilime.com>
2019-01-21 10:36:00 +0100

    Read etcd configuration from env
    - make db and notification driver configurable
    - read etcd configuration from env and write to configuration files
      of services that use etcd
    
    Co-authored-by: Daniel Furman <daniel.furman@codilime.com>
    Co-authored-by: Tomasz Młynarczyk <tomasz.mlynarczyk@codilime.com>
    Depends-on: If139877b91dc935d63676a11517d75a7cf2a9b7a
    Depends-on: I1238b5f21e1500fa276ddd3a342864be03be95d7
    Change-Id: I00d05eb5a5d59482639e0634296916db42b55ff9
    partial-jira-bug: JBE-783

--- containers/controller/config/schema/entrypoint.sh
+++ containers/controller/config/schema/entrypoint.sh
@@ -16,12 +16,15 @@ api_server_port=$CONFIG_API_PORT
 log_file=$LOG_DIR/contrail-schema.log
 log_level=$LOG_LEVEL
 log_local=$LOG_LOCAL
+db_driver=$DB_DRIVER
+notification_driver=$NOTIFICATION_DRIVER
 cassandra_server_list=$cassandra_server_list
 zk_server_ip=$ZOOKEEPER_SERVERS
 
 rabbit_server=$RABBITMQ_SERVERS
 $rabbit_config
 $kombu_ssl_config
+$etcd_config
 
 collectors=$COLLECTOR_SERVERS
 
