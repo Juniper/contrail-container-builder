b58d67ac9e7e32e4889fcfdf0fb6af40e078c41b
Andrey Pavlov <andrey.mp@gmail.com>
2019-01-30 10:16:53 +0300

    extract devicemgr to separate POD
    original devicemgr container will be removed after changes in
    contrail-ansible-deploer
    
    Change-Id: I33e543f79a9ab9e582ec9337b33f79688a2ba894
    Depends-On: Ib49b682b8ce7b7f22f34e5749d77bd9da1fa8bf6
    partial-jira-bug: JCB-218801

--- containers/status/contrail-status.py
+++ containers/status/contrail-status.py
@@ -43,7 +43,11 @@ CONTRAIL_SERVICES_TO_SANDESH_SVC = {
         'api': 'contrail-api',
         'schema': 'contrail-schema',
         'svc-monitor': 'contrail-svc-monitor',
+    },
+    'device-manager': {
+        'nodemgr': 'contrail-device-manager-nodemgr',
         'device-manager': 'contrail-device-manager',
+        'dnsmasq': None,
     },
     'config-database': {
         'nodemgr': 'contrail-config-database-nodemgr',
@@ -85,8 +89,6 @@ CONTRAIL_SERVICES_TO_SANDESH_SVC = {
         'plugin': None,
     }
 }
-
-# TODO: Include vcenter-plugin
 
 
 debug_output = False
