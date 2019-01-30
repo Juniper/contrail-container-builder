8cb6667472dbe545122e2008e44fed8f888dde43
Andrey Pavlov <andrey.mp@gmail.com>
2019-01-30 10:16:53 +0300

    extract devicemgr to separate POD
    original devicemgr container will be removed after changes in
    contrail-ansible-deploer
    
    Change-Id: I33e543f79a9ab9e582ec9337b33f79688a2ba894
    Depends-On: Ib49b682b8ce7b7f22f34e5749d77bd9da1fa8bf6
    partial-jira-bug: JCB-218801

---
+++ containers/controller/devicemgr/entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+source /common.sh
+
+pre_start_init
+
+host_ip=$(get_listen_ip_for_node DEVICE_MANAGER)
+cassandra_server_list=$(echo $CONFIGDB_SERVERS | sed 's/,/ /g')
+
+cat > /etc/contrail/contrail-device-manager.conf << EOM
+[DEFAULTS]
+host_ip=${host_ip}
+http_server_ip=$(get_introspect_listen_ip_for_node DEVICE_MANAGER)
+api_server_ip=$CONFIG_NODES
+api_server_port=$CONFIG_API_PORT
+analytics_server_ip=$ANALYTICS_NODES
+analytics_server_port=$ANALYTICS_API_PORT
+push_mode=1
+log_file=$LOG_DIR/contrail-device-manager.log
+log_level=$LOG_LEVEL
+log_local=$LOG_LOCAL
+cassandra_server_list=$cassandra_server_list
+cassandra_use_ssl=${CASSANDRA_SSL_ENABLE,,}
+cassandra_ca_certs=$CASSANDRA_SSL_CA_CERTFILE
+zk_server_ip=$ZOOKEEPER_SERVERS
+
+# configure directories for job manager
+# the same directories must be mounted to dnsmasq and DM container
+dnsmasq_conf_dir=/etc/dnsmasq
+tftp_dir=/etc/tftp
+dhcp_leases_file=/var/lib/misc
+
+rabbit_server=$RABBITMQ_SERVERS
+$rabbit_config
+$kombu_ssl_config
+
+collectors=$COLLECTOR_SERVERS
+
+$sandesh_client_config
+
+$collector_stats_config
+EOM
+
+add_ini_params_from_env DEVICE_MANAGER /etc/contrail/contrail-device-manager.conf
+
+set_third_party_auth_config
+set_vnc_api_lib_ini
+
+exec "$@"
