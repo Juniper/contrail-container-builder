c6793c0720ddc081cb47c0a5f9a9c8258afac2e8
jnpr-rtulsian <rtulsian@juniper.net>
2019-02-06 11:50:16 -0800

    Fixed leases file arg and added dnsmasq base config
    Change-Id: I23bbf0143bfb583a0b6c6fa449a0209e5c7cf350
    partial-jira-bug: JCB-219115

--- containers/external/dnsmasq/entrypoint.sh
+++ containers/external/dnsmasq/entrypoint.sh
@@ -1,3 +1,18 @@
 #!/bin/bash -e
 
+cat > /etc/dnsmasq/base.conf << EOM
+log-facility=/var/log/contrail/dnsmasq.log
+keep-in-foreground
+bogus-priv
+log-dhcp
+EOM
+if is_enabled ${ENABLE_TFTP} ; then
+cat > /etc/dnsmasq/base.conf << EOM
+enable-tftp
+tftp-root=/etc/tftp
+EOM
+fi
+
+cat /dev/null > /var/lib/dnsmasq/dnsmasq.leases
+
 exec "$@"
