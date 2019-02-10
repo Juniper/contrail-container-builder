71c15d70d8754b9146cb39f2f7e1fc881877c0cd
Kumar Harsh <hkumar@juniper.net>
2019-02-10 08:10:25 +0000

    Add support for tor agent container . contrail-tor-agent binary is part of contrail-vrouter-agent package Add docker file for container provision physical switch and tor agent virtual router object for each container. Multiple tor agent containers will run on a tor service node, so adding of physical device and virtual router is done in entrypoint Initial review request, Todo: sumit a requirement doc
    Change-Id: I4abd920edba0d2d742eb92a0da7b0d27ee44743a
    closes-jira-bug:CEM-99

--- containers/vrouter/kernel-init/entrypoint.sh
+++ containers/vrouter/kernel-init/entrypoint.sh
@@ -16,4 +16,14 @@ for k_dir in `ls -d /lib/modules/*` ; do
 done
 depmod -a
 
+# OPTIONAL vrouter limit parameter
+vrouter_params="options vrouter"
+mpls_labels="vr_mpls_labels=$VR_MPLS_LABELS"
+nexthops="vr_nexthops=$VR_NEXTHOPS"
+vrfs="vr_vrfs=$VR_VRFS"
+macs="vr_bridge_entries=$VR_BRIDGE_ENTRIES"
+flow_entries="vr_flow_entries=$VR_FLOW_ENTRIES"
+vrouter_params="$vrouter_params $mpls_labels $nexthops $vrfs $macs $flow_entries"
+echo ${vrouter_params} > /etc/modprobe.d/vrouter.conf
+
 exec $@
