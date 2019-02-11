309bf5be6dc15b266375a865a9afcf14389d58d5
Andrey Pavlov <andrey.mp@gmail.com>
2019-02-11 14:31:55 +0300

    WIP: make container OpenStack version-less
    TODO: mirrors support
    
    Change-Id: I444f7bc671a6f5fd60aced478dcb58ac09ac3c73

---
+++ containers/openstack/neutron-init/_prepare_packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+# download all requried version of package python-neutron-lbaas
+# appropriate version based on OPENSTACK_VERSION will be chosen and installed
+
+mkdir -p /opt/packages
+for os_rpm in $(echo $OPENSTACK_EXTRA_RPMS | tr -d '"' | tr ',' ' ') ; do
+  echo "INFO: Using $os_rpm"
+  yum downgrade $os_rpm || /bin/true
+  yum upgrade $os_rpm || /bin/true
+  url=$(repoquery --location python-neutron-lbaas)
+  if [[ -z "$url" ]]; then
+    echo "ERROR: python-neutron-lbaas couldn't be found in repo $os_rpm"
+    exit 1
+  fi
+  pkg_name=$(basename $url)
+  curl -s -o /opt/packages/$basename $url
+done
