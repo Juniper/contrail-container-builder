7ce640930f99c014fc359e36c6b96df4b23247a6
Dmitry Eremeev <mitossvyaz@mail.ru>
2019-02-25 15:40:16 +0300

    initial_commit
    Change-Id: I506b9d57c66fb214e5744264587a1bea7c84c012
    closes-jira-bug: JBE-873

---
+++ containers/analytics/json2sandesh/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source /common.sh
+
+pre_start_init
+
+host_ip=$(get_listen_ip_for_node ANALYTICS)
+
+cat > /etc/contrail/contrail-json2sandesh.conf << EOM
+[INIT_GENERATOR]
+instance_id=99
+collectors=$COLLECTOR_SERVERS
+log_level=$LOG_LEVEL
+log_file=$LOG_DIR/contrail-json2sandesh.log
+enable_syslog=$LOG_LOCAL
+syslog_facility=
+sandesh_send_rate_limit=100
+
+[INTERFACE_CONFIG]
+api_host=${host_ip}
+api_port=8113
+api_debug=True
+EOM
+
+add_ini_params_from_env ANALYTICS_JSON2SANDESH /etc/contrail/contrail-json2sandesh.conf
+
+exec "$@"
