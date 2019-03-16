12ca7205c661d30c0e419e3528d99850f5820cc4
Andrey Pavlov <andrey.mp@gmail.com>
2019-03-16 09:42:41 -0700

    make container OpenStack version-less
    this patchset makes neutron-init container as universal container
    that can be used with newton, ocata, queens, rocky and next openstack
    versions.
    
    closes-jira-bug: JCB-179820
    Change-Id: I7b652df78d9b3ac95d31036264a25f8c3ab3eb95

--- containers/openstack/neutron-init/entrypoint.sh
+++ containers/openstack/neutron-init/entrypoint.sh
@@ -1,11 +1,45 @@
 #!/bin/bash -ex
 
 # linux distro here always centos for now
-src_path='/usr/lib/python2.7/site-packages'
+lib_path='/usr/lib/python2.7/site-packages'
 
-mkdir -p /opt/plugin/site-packages
-for module in neutron_plugin_contrail vnc_api cfgm_common neutron_lbaas ; do
+# some containers with neutron-server have neutron_lbaas and some don't.
+# and we can't check presense of this library inside neutron-server in init contrainer.
+# due to this fact we may need to bring own version of neutron-lbaas into neutron-server container.
+# rhel container with neuton-server has this package installed and therefore we don't need to bring own.
+# try to find stored package with python-neutron-lbaas-$OPENSTACK_VERSION, install it and copy to /opt/plugin/site-packages
+
+echo "INFO: passed OPENSTACK_VERSION is $OPENSTACK_VERSION"
+if [[ -z "$OPENSTACK_VERSION" ]]; then
+  echo "ERROR: OPENSTACK_VERSION is required to init neutron plugin correctly"
+  exit 1
+fi
+
+function copy_sources() {
+  local src_path=$1
+  local module=$2
   for item in `ls -d $src_path/${module}*` ; do
     cp -r $item /opt/plugin/site-packages/
   done
+}
+
+mkdir -p /opt/plugin/site-packages
+for module in neutron_plugin_contrail vnc_api cfgm_common ; do
+  copy_sources $lib_path $module
 done
+
+# install appropriate version of python-neutron-lbaas based on OPENSTACK_VERSION
+pkg=$(ls -1 /opt/packages/ | grep python-neutron-lbaas-$OPENSTACK_VERSION)
+if [[ -z "$pkg" ]]; then
+  # rhel case
+  echo "INFO: package couldn't be found for this version: $pkg_version. Available packages:"
+  ls -l /opt/packages/
+else
+  mkdir -p /tmp/lbaas
+  pushd /tmp/lbaas
+  # rpm install can't be used here cause this container can be run under non-root user
+  rpm2cpio $pkg | cpio -idmv
+  copy_sources usr/lib/python2.7/site-packages neutron_lbaas
+  popd
+  rm -rf /tmp/lbaas
+fi
