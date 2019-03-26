0445365c2185280fb8cef54d6fc2df91c26c9012
Dmitry_Eremeev <deremeev@juniper.net>
2019-03-26 12:44:33 +0300

    TF usage reporting client.
    Change-Id: I9d49a005cf8fbfbee8784e62781c0b85d1096782
    closes-jira-bug: CEM-4219

---
+++ containers/controller/config/stats-client/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+source /common.sh
+
+pre_start_init
+
+export STATS_SERVER=https://tungsten.io:8000/api/stats
+
+set_third_party_auth_config
+set_vnc_api_lib_ini
+
+cat > /etc/contrail/contrail-stats-client.conf << EOM
+[LOGGING]
+log_file=$LOG_DIR/contrail-stats-client.log
+log_level=SYS_INFO
+EOM
+
+add_ini_params_from_env STATS_CLIENT /etc/contrail/contrail-stats-client.conf
+
+while true; do
+  /usr/bin/python /usr/bin/contrail-stats-client --config /etc/contrail/contrail-stats-client.conf
+  sleep 1h
+done
