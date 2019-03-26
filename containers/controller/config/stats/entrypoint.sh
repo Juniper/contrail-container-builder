5a4e9c8302f87a1229b17e94eb714398bc58e954
Dmitry_Eremeev <deremeev@juniper.net>
2019-03-26 12:44:33 +0300

    TF usage reporting client.
    Link to server source code - https://github.com/progmaticlab/tf_stats_server
    Effort to massively offer Tungsten Fabric as an open-source SDN standard is quite new and require measuring the progress of “conquering” the market - growth rate, community “health”.
    The service collects statistics information and sends it to statistics server.
    By default the service doesn't collect and send statistics.
    Sending settings are checked every hour.
    To enable(edit) sending:
    - go to contrail web UI (https://ip_address:8143) --> Configure tab --> Tags --> Global Tags
    ( https://ip_address:8143/#p=config_tags_globaltags )
    - add tag with type = Label
    valid values are:
    1. stats_monthly
    2. stats_weekly
    3. stats_daily
    In order to disable sending just remove tags.
    In case there are few tags (misconfiguration) the service picks a tag with the least frequency
    "stats_monthly" is the least frequency tag.
    
    Change-Id: I8434cec969ba350262f3181fb9d0815503cc184b
    closes-jira-bug: CEM-4219
    Depends-On: I10f6341a468e5d04e1208638a172eff16b59d214

---
+++ containers/controller/config/stats/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+source /common.sh
+
+pre_start_init
+
+set_vnc_api_lib_ini
+
+cat > /etc/contrail/contrail-stats-client.conf << EOM
+[DEFAULT]
+stats_server=${STATS_SERVER:-"http://stats.tungsten.io/api/stats"}
+log_file=$LOG_DIR/contrail-stats-client.log
+log_level=$LOG_LEVEL
+EOM
+
+add_ini_params_from_env STATS /etc/contrail/contrail-stats-client.conf
+
+run_service "$@"
