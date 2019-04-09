9fab26dc476c0abda034a6beb6d571b46538a943
Andrey Pavlov <andrey-mp@yandex.ru>
2019-04-09 22:51:52 +0300

    create agent.conf in mounted folder for dpdk case for vif tool
    for kernel case it's not needed - kernel mode is default value
    this change also requires changes in orchestrators - one more mount should be added
    
    Change-Id: I8d80b29dec969260e714b4e8ba111a3767b767e5
    partial-jira-bug: CEM-3060

--- containers/vrouter/kernel-init-dpdk/entrypoint.sh
+++ containers/vrouter/kernel-init-dpdk/entrypoint.sh
@@ -4,5 +4,7 @@ source /common.sh
 source /agent-functions.sh
 
 copy_agent_tools_to_host
+# the following func requires mount /etc/contrail from host to /host/etc/contrail in container
+prepare_vif_config $AGENT_MODE /host
 
 exec $@
