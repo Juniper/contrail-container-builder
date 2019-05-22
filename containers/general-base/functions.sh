65d02009c0324e9992589291dbe02272b6c80ff2
alexey-mr <alexey.morlang@gmail.com>
2019-05-22 21:29:45 +0300

    Fix route resolving for local IPs
    Change-Id: If28fea43a0be137f20b31a7b0622257537e74c4e
    closes-jira-bug: CEM-5458
    (cherry picked from commit 3024941e4086e46d74c49665282f5845017d3c3c)

--- containers/general-base/functions.sh
+++ containers/general-base/functions.sh
@@ -23,7 +23,12 @@ function get_gateway_nic_for_ip() {
     local iface=$(ip route get $ip | grep -o "dev.*" | awk '{print $2}')
     if [[ "$iface" == 'lo' ]] ; then
       # ip is belong to this machine
-      iface=$(ip address show | grep -m 1 -F "inet ${ip}/" | awk '{print($NF)}')
+      # Workaround for the case of Openshift with single NIC
+      # on compute node (that is used for vhost0 in kernel mode):
+      # dhclient may be still running and get an IP for the initial NIC
+      # https://contrail-jws.atlassian.net/browse/JCB-219329
+      iface=$(ip address show | grep -F "inet ${ip}/" | awk '{print($NF)}' | grep -m 1 vhost0)
+      [ -z "$iface" ] && iface=$(ip address show | grep -m 1 -F "inet ${ip}/" | awk '{print($NF)}')
     fi
     echo $iface
 }
