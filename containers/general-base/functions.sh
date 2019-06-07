184a741c3a8f56cd315fafabbe52e15825126d42
alexey-mr <alexey.morlang@gmail.com>
2019-06-07 11:29:07 +0300

    Allow contrail to access logs and run control as root
    - in rhosp rabbit and other containers
    are run udner different from contrail user.
    And they save the same logs folder.
    
    - run control service as root as it opens bgp port
    
    - chown contrail /opt/contrail for device manager
    
    Change-Id: I9139787a63e638ced2199a1f3b1207b6de2be401
    Closes-jira-bug: CEM-6373

--- containers/general-base/functions.sh
+++ containers/general-base/functions.sh
@@ -135,14 +135,21 @@ function resolve_hostname_by_ip() {
 }
 
 function run_service() {
-  if [[ -n "$CONTRAIL_UID" &&  "$(id -u)" = '0' ]] ; then
+  if [[ -n "$CONTRAIL_UID" && -n "$CONTRAIL_GID" &&  "$(id -u)" = '0' ]] ; then
+    local owner_opts="$CONTRAIL_UID:$CONTRAIL_GID"
+    
     mkdir -p /var/log/contrail
-    chown $CONTRAIL_UID /var/log/contrail
-    chmod 755 /var/log/contrail
+    # change files only with root
+    #   in some cases rabbit, redis and other services
+    #   may kepp logs there under their users
+    chown $owner_opts /var/log/contrail
+    find /var/log/contrail -uid 0 -exec chown $owner_opts {} + ;
+    chmod 777 /var/log/contrail
 
     mkdir -p /etc/contrail
-    chown $CONTRAIL_UID /etc/contrail
-    chmod 755 -R /etc/contrail
+    chown $owner_opts /etc/contrail
+    find /etc/contrail -uid 0 -exec chown $owner_opts {} + ;
+    chmod 755 /etc/contrail
 
     export HOME=/home/contrail
     exec setpriv --reuid $CONTRAIL_UID --regid $CONTRAIL_GID --clear-groups --no-new-privs "$@"
