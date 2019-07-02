6474249f138c0003031efde0820350c42c0d9e6b
Adrian Szczepański <adrian.szczepanski@codilime.com>
2019-07-02 09:21:38 +0200

    WIP: CVFM SSL Support
    Change-Id: Iecc8c7c9bd822970d0f296f6c80eab73b87f46b6

--- containers/vcenter/fabric-manager/entrypoint.sh
+++ containers/vcenter/fabric-manager/entrypoint.sh
@@ -2,44 +2,50 @@
 
 source /common.sh
 
-cat > /etc/contrail/contrail-vcenter-fabric-manager/config.yaml << EOM
-vcenter:
-  host: $VCENTER_SERVER
-  port: ${VCENTER_PORT:-443}
-  username: $VCENTER_USERNAME
-  password: $VCENTER_PASSWORD
-  preferred_api_versions:
-    - ${VCENTER_API_VERSION:-vim.version.version10}
-  datacenter: $VCENTER_DATACENTER
+cat > /etc/contrail/contrail-vcenter-fabric-manager/cvfm.conf << EOM
+[DEFAULTS]
+host_ip=$(get_listen_ip_for_node VCENTER_FABRIC_MANAGER)
+http_server_ip=$(get_introspect_listen_ip_for_node VCENTER_FABRIC_MANAGER)
 
-vnc:
-  api_server_host: $CONFIG_NODES
-  api_server_port: $CONFIG_API_PORT
-  auth_host: $KEYSTONE_AUTH_HOST
-  auth_port: $KEYSTONE_AUTH_PUBLIC_PORT
-  username:  $KEYSTONE_AUTH_ADMIN_USER
-  password:  $KEYSTONE_AUTH_ADMIN_PASSWORD
-  tenant_name: $KEYSTONE_AUTH_ADMIN_TENANT
-  proto: $KEYSTONE_AUTH_PROTO
-  insecure: $KEYSTONE_AUTH_INSECURE
-  certfile: $KEYSTONE_AUTH_CERTFILE
-  keyfile: $KEYSTONE_AUTH_KEYFILE
-  cafile: $KEYSTONE_AUTH_CA_CERTFILE
-  auth_tokens: $KEYSTONE_AUTH_URL_TOKENS
-  fabric_name: ${FABRIC_NAME:-vCenter-fabric}
+[VCENTER]
+vc_host=$VCENTER_SERVER
+vc_port=${VCENTER_PORT:-443}
+vc_username=$VCENTER_USERNAME
+vc_password=$VCENTER_PASSWORD
+vc_preferred_api_versions=${VCENTER_API_VERSION:-vim.version.version10}
+vc_datacenter=$VCENTER_DATACENTER
 
-sandesh:
-  collectors: $COLLECTOR_SERVERS
-  introspect_port: 9099
-  logging_level: $LOG_LEVEL
-  log_file: $LOG_DIR/contrail-vcenter-fabric-manager.log
-  http_server_ip: $(get_introspect_listen_ip_for_node VCENTER_FABRIC_MANAGER)
-  introspect_ssl_enable: $INTROSPECT_SSL_ENABLE
-  sandesh_ssl_enable: $SANDESH_SSL_ENABLE
-  keyfile: $SANDESH_KEYFILE
-  certfile: $SANDESH_CERTFILE
-  cafile: $SANDESH_CA_CERTFILE
+[VNC]
+api_server_host=$CONFIG_NODES
+api_server_port=$CONFIG_API_PORT
+api_server_use_ssl=$SSL_ENABLE
+api_server_insecure=$SSL_INSECURE
+api_keyfile=$SERVER_KEYFILE
+api_certfile=$SERVER_CERTFILE
+api_cafile=$SERVER_CA_CERTFILE
+fabric_name=${FABRIC_NAME:-vCenter-fabric}
+
+$sandesh_client_config
+
+[INTROSPECT]
+collectors=$COLLECTOR_SERVERS
+logging_level=$LOG_LEVEL
+log_file=$LOG_DIR/contrail-vcenter-fabric-manager.log
+introspect_port=9099
+
+[ZOOKEEPER]
+zookeeper_servers=$ZOOKEEPER_SERVERS
 
 EOM
+
+if [[ $AUTH_MODE == "keystone" ]]; then
+    cat >> /etc/contrail/contrail-vcenter-fabric-manager/cvfm.conf << EOM
+[AUTH]
+auth_user=${KEYSTONE_AUTH_ADMIN_USER:-''}
+auth_password=${KEYSTONE_AUTH_ADMIN_PASSWORD:-''}
+auth_tenant=${KEYSTONE_AUTH_ADMIN_TENANT:-''}
+auth_token_url=$KEYSTONE_AUTH_PROTO://${KEYSTONE_AUTH_HOST}:${KEYSTONE_AUTH_ADMIN_PORT}${KEYSTONE_AUTH_URL_TOKENS}
+EOM
+fi
 
 exec "$@"
