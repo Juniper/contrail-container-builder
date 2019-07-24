ec0f2f02128a07070ff937c112248773f3552483
Krzysztof Gromadzki <krzysztof.gromadzki@codilime.com>
2019-07-24 15:33:50 +0200

    Run vcenter-fabric-manager container under non-root user
    Removed unused variables
    Updated PARAMS.md
    
    Change-Id: I72e0c9efacfdb073609beaf6ce4feba34707cbfc
    Partial-Jira-Bug: CEM-7439

--- containers/vcenter/fabric-manager/entrypoint.sh
+++ containers/vcenter/fabric-manager/entrypoint.sh
@@ -23,7 +23,6 @@ api_server_insecure=$SSL_INSECURE
 api_keyfile=$SERVER_KEYFILE
 api_certfile=$SERVER_CERTFILE
 api_cafile=$SERVER_CA_CERTFILE
-fabric_name=${FABRIC_NAME:-vCenter-fabric}
 
 $sandesh_client_config
 
@@ -31,7 +30,6 @@ $sandesh_client_config
 collectors=$COLLECTOR_SERVERS
 logging_level=$LOG_LEVEL
 log_file=$LOG_DIR/contrail-vcenter-fabric-manager.log
-introspect_port=9099
 
 [ZOOKEEPER]
 zookeeper_servers=$ZOOKEEPER_SERVERS
@@ -48,4 +46,4 @@ auth_token_url=$KEYSTONE_AUTH_PROTO://${KEYSTONE_AUTH_HOST}:${KEYSTONE_AUTH_ADMI
 EOM
 fi
 
-exec "$@"
+run_service "$@"
