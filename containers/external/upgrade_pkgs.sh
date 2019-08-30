cd1f10c0510c7f616a3270e70689e18c71eb2d6f
alexey-mr <alexey.morlang@gmail.com>
2019-08-30 14:04:08 +0300

    Updates packages for external & ubuntu based
    - this is to fix security volnurabilities
    
    partial-jira-bug: CEM-8477
    partial-jira-bug: CEM-8582
    close-jira-bug: CEM-8583
    Change-Id: I5a682b14508309d842b47e15491cc6c04c4c0312

---
+++ containers/external/upgrade_pkgs.sh
@@ -0,0 +1,14 @@
+  #!/bin/bash -x
+
+default_pkgs="apt,procps,util-linux,systemd"
+
+pkgs=${1:-${default_pkgs}}
+echo "INFO: Upgrade $pkgs"
+pkgs="${pkgs//,/ }"
+
+DEBIAN_FRONTEND=noninteractive
+apt-get update || /bin/true
+apt-get upgrade -y --no-install-recommends $pkgs
+apt-get autoremove -y
+apt-get clean
+rm -rf /var/lib/apt/lists/*
