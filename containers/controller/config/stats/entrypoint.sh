2070e4a2e69cb182b6dfb8788992ba1caa1170ae
Dmitry_Eremeev <deremeev@juniper.net>
2019-09-17 20:27:56 +0300

    fix for access rights of state file in docker container of stats client
    Change-Id: I804c87f146aa02c62a9b913a85cd000abd03de9e
    
    closes-jira-bug: CEM-4219
    
    Depends-On: I43984bf791bd46530dbf6e41f597a000650707e4
    Change-Id: I36602a4ad3817d21a42812ffe47cbee04fec2cd6
    (cherry picked from commit 52e2225cf499ef044069ad4db4024f6d92409669)

--- containers/controller/config/stats/entrypoint.sh
+++ containers/controller/config/stats/entrypoint.sh
@@ -6,13 +6,19 @@ pre_start_init
 
 set_vnc_api_lib_ini
 
+stats_data=/var/lib/stats
+
 cat > /etc/contrail/contrail-stats-client.conf << EOM
 [DEFAULT]
 stats_server=${STATS_SERVER:-"http://stats.tungsten.io/api/stats"}
 log_file=$LOG_DIR/contrail-stats-client.log
 log_level=$LOG_LEVEL
+state=$stats_data/state
 EOM
 
 add_ini_params_from_env STATS /etc/contrail/contrail-stats-client.conf
+
+mkdir -p $stats_data
+chown -R $CONTRAIL_UID:$CONTRAIL_GID $stats_data
 
 run_service "$@"
