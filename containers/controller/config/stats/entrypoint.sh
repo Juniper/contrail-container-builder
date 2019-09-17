c4b4de2a76db9eff76f1abe35e4b58b27644c173
Dmitry_Eremeev <deremeev@juniper.net>
2019-09-17 20:27:56 +0300

    fix for access rights of cache file in docker container of stats client
    Change-Id: I804c87f146aa02c62a9b913a85cd000abd03de9e

--- containers/controller/config/stats/entrypoint.sh
+++ containers/controller/config/stats/entrypoint.sh
@@ -6,13 +6,17 @@ pre_start_init
 
 set_vnc_api_lib_ini
 
+cache_dir=/var/lib/$CONTRAIL_USER
+
 cat > /etc/contrail/contrail-stats-client.conf << EOM
 [DEFAULT]
 stats_server=${STATS_SERVER:-"http://stats.tungsten.io/api/stats"}
 log_file=$LOG_DIR/contrail-stats-client.log
 log_level=$LOG_LEVEL
+cache=$cache_dir/cache
 EOM
 
-add_ini_params_from_env STATS /etc/contrail/contrail-stats-client.conf
+mkdir -p $cache_dir
+chown -R $CONTRAIL_UID:$CONTRAIL_GID $cache_dir
 
 run_service "$@"
