163339182ed695ac1ad4da887449c910e3e2158d
eshapovalov <eshapovalov.dev@gmail.com>
2019-09-20 15:19:47 +0300

    Building from source for Config containers
    Description: Add capability to build from sources for api,base,schema and devicemgr containers
    Change-Id: I51af365633c3a3ebe4465e5bcf2ec3415480a3b0
    partial-jira-bug: CEM-8885
    Depends-on: I2545dd22d2d1523f9079903064609bc1fab91203

---
+++ containers/controller/config/svcmonitor/build_src/setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+sed -e '/^tsflags=nodocs/d' -i /etc/yum.conf
+
+build_path="/build_src"
+
+if [[ -f ${build_path}/.src ]]; then
+ src_items=$(cat "${build_path}/.src" | sed '/^$/d' | tr '\n' ',')
+ CONTRAIL_COMPONENTS=$src_items
+fi
+build_root=${1:-${CONTRAIL_SOURCE//\"/}}
+
+components=${2:-${CONTRAIL_COMPONENTS//\"/}}
+
+[ -e "/contrail-setup-common.sh" ] && source /contrail-setup-common.sh
+
+function log() {
+  echo "INFO: CONFIG API: $@"
+}
+
+function setup_user() {
+  local path="$1"
+  local mode=${2:-"0744"}
+  [[  -n "$CONTRAIL_UID" && \
+      -n "$CONTRAIL_GID" &&  \
+      "$(id -u)" = '0' ]] && chown -R $CONTRAIL_UID:$CONTRAIL_GID $path
+  [[ -n $"mode" ]] && chmod -R $mode $path
+}
+
+CONTRAIL_DEPS=''
+[ -e ${build_path}/.deps ] && CONTRAIL_DEPS+=$(cat ${build_path}/.deps)
+[ -e ${build_path}/.deps.$LINUX_DISTR ] && CONTRAIL_DEPS+="\n$(cat ${build_path}/.deps.$LINUX_DISTR)"
+CONTRAIL_DEPS=$(echo -e "$CONTRAIL_DEPS" | sed '/^$/d' | sort | uniq | tr '\n' ',')
+CONTRAIL_DEPS=${CONTRAIL_DEPS%%//,}
+CONTRAIL_DEPS=${CONTRAIL_DEPS##//,}
+CONTRAIL_DEPS=$(echo ${CONTRAIL_DEPS//,/ } | tr -d '"' | sort | uniq)
+if [[ -n "$CONTRAIL_DEPS" ]] ; then
+  time yum update all -y
+  time yum install -y $CONTRAIL_DEPS
+fi
+
+
+if [[ -z "$build_root" ]] ; then
+  log "No source code provided, nothing to do"
+  exit 0
+fi
+
+log "Build root path $build_root"
+
+cd $build_root
+
+for src_folder in ${components//,/ } ; do
+  pushd $src_folder
+  time python setup.py install --root=/
+  exitcode=$?
+  log "Setup.py exitcode is finished with exitcode ${exitcode}"
+  if [[ $exitcode -ne 0 ]]; then
+   log "Setup.py within ${src_folder} finished with error"
+   exit 1
+  fi
+  popd
+done
+
+for i in /var/lib/contrail /var/log/contrail ; do
+  mkdir -p $i
+  setup_user $i
+done
+
+yum clean all -y
+rm -rf /var/cache/yum
