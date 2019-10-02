f1e2845df87a5ffa3c348407744ebc995021ba41
Andrey Pavlov <andrey-mp@yandex.ru>
2019-10-01 17:23:36 -0700

    add keystone conf to stats container
    Change-Id: I2f939adc1cd0d5706db846c3a0165ea28ce1cd57
    closes-jira-bug: CEM-9365

--- containers/controller/config/stats/entrypoint.sh
+++ containers/controller/config/stats/entrypoint.sh
@@ -4,9 +4,9 @@ source /common.sh
 
 pre_start_init
 
-set_vnc_api_lib_ini
-
 stats_data=/var/lib/stats
+mkdir -p $stats_data
+chown -R $CONTRAIL_UID:$CONTRAIL_GID $stats_data
 
 cat > /etc/contrail/contrail-stats-client.conf << EOM
 [DEFAULT]
@@ -18,7 +18,7 @@ EOM
 
 add_ini_params_from_env STATS /etc/contrail/contrail-stats-client.conf
 
-mkdir -p $stats_data
-chown -R $CONTRAIL_UID:$CONTRAIL_GID $stats_data
+set_third_party_auth_config
+set_vnc_api_lib_ini
 
 run_service "$@"
