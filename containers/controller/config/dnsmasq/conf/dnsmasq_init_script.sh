d9fbadf4597429a4c347b691ae70db1f5907a86e
Michael Henkel <mhenkel@juniper.net>
2019-11-08 11:22:52 -0800

    fixes script path
    closes-jira-bug: CEM-10371
    Change-Id: I41eda65fbdb8fb0a10a7e7691398d37b144fc7f6

--- containers/controller/config/dnsmasq/conf/dnsmasq_init_script.sh
+++ containers/controller/config/dnsmasq/conf/dnsmasq_init_script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 if [ ${1} = init ] ; then
   echo "SCRIPT INIT"
-  cd /etc/dnsmasq
+  cd /etc/scripts
   python dnsmasq_lease_processing.py read
 else
   if [ ${1} = del ] ; then
     echo "SCRIPT DELETE"
-    cd /etc/dnsmasq
+    cd /etc/scripts
     python dnsmasq_lease_processing.py delete ${3}
   fi
   if [ ${1} = old ] || [ ${1} = add ] ; then
     echo "SCRIPT ADD"
-    cd /etc/dnsmasq
+    cd /etc/scripts
     python dnsmasq_lease_processing.py write ${2} ${3} ${4} ${DNSMASQ_LEASE_EXPIRES}
   fi
 fi
