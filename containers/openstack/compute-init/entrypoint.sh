33718825e97646bd8404ce78180c8a6a42e6f865
Andrey Pavlov <andrey-mp@yandex.ru>
2019-11-09 18:35:10 +0300

    make openstack containers smaller and simpler
    it needs to rework Juju pluginisation
    
    Change-Id: Ifb865eb8164453549e637b4a6e98066de9a94ac8
    partial-jira-bug: CEM-10417

--- containers/openstack/compute-init/entrypoint.sh
+++ containers/openstack/compute-init/entrypoint.sh
@@ -1,15 +1,6 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-set -ex
 mkdir -p /opt/plugin/bin
-cp /opt/contrail/bin/vrouter-port-control /opt/plugin/bin/
-
-# linux distro here always centos for now
-src_path='/usr/lib/python2.7/site-packages'
-
+cp /opt/contrail/bin/* /opt/plugin/bin/
 mkdir -p /opt/plugin/site-packages
-for module in vif_plug_vrouter vif_plug_contrail_vrouter nova_contrail_vif ; do
-  for item in `ls -d $src_path/${module}*` ; do
-    cp -r $item /opt/plugin/site-packages/
-  done
-done
+cp -rf /opt/contrail/site-packages/* /opt/plugin/site-packages/
--- containers/openstack/heat-init/entrypoint.sh
+++ containers/openstack/heat-init/entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/bash -ex
 
-# linux distro here always centos for now
-src_path='/usr/lib/python2.7/site-packages'
-
 mkdir -p /opt/plugin/site-packages
-for module in contrail_heat vnc_api cfgm_common ; do
-  for item in `ls -d $src_path/${module}*` ; do
-    cp -r $item /opt/plugin/site-packages/
-  done
-done
+cp -rf /opt/contrail/site-packages/* /opt/plugin/site-packages/
