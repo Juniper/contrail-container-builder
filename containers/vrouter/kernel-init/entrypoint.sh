90ac754af219a90d88aba30fb0d396b2cf2399f2
eshapovalov <eshapovalov.dev@gmail.com>
2019-11-27 19:50:03 +0300

    Build from source for vrouter containers
    partial-jira-bug: CEM-10902
    
    Depends-on: Ic5baf07f81c5f93a9e7f1dbb80907bbf3851a975
    Change-Id: I2dc8f2177854d4f093863b1a0205a2259db5ece9

--- containers/vrouter/kernel-init/entrypoint.sh
+++ containers/vrouter/kernel-init/entrypoint.sh
@@ -7,9 +7,7 @@ source /agent-functions.sh
 
 copy_agent_tools_to_host
 
-kver=`uname -r | awk -F "-" '{print $1}'`
-echo "INFO: copy kernel module for kver=${kver} to host"
-modfile=`ls -1rt /opt/contrail/vrouter-kernel-modules/${kver}-*/vrouter.ko | tail -1`
+modfile="/opt/contrail/vrouter-kernel-modules/vrouter.ko"
 for k_dir in `ls -d /lib/modules/*` ; do
   mkdir -p ${k_dir}/kernel/net/vrouter
   cp -f ${modfile} ${k_dir}/kernel/net/vrouter
