c25148c87da00724eae86ff83fc87a611ac63d2d
ameerm <ameerm@mellanox.com>
2019-12-16 13:10:58 +0200

    Updated mellanox plugin redhat version to 7.7
    Change-Id: I38d9c2530bb360c7fd625fe4fea449bb2493f451
    Signed-off-by: ameerm <ameerm@mellanox.com>

--- containers/vrouter/plugin/mellanox/init/redhat/entrypoint.sh
+++ containers/vrouter/plugin/mellanox/init/redhat/entrypoint.sh
@@ -2,6 +2,45 @@
 
 # /lib/modules needs to be mounted.
 
-yum --nogpgcheck -y install mlnx-ofed-dpdk-upstream-libs
+conf_enable()
+{
+  local mstdev=$1
+  local config=$2
+
+  mstconfig -d $mstdev --yes set ${config}=1
+}
+
+conf_query()
+{
+  local mstdev=$1
+  local config=$2
+
+  mstconfig -d $mstdev query ${config} 2> /dev/null | grep -w ${config} | awk '{print $NF}'
+}
+
+dev_reset()
+{
+  local mstdev=$1
+
+  mstfwreset -d $mstdev --yes reset
+}
+
+for mlnx_device in `lspci -d 15b3: 2> /dev/null | awk '{print $1}'`
+do
+  case ${mlnx_device} in
+    *\.0)
+      val=`conf_query ${mlnx_device} "FLEX_PARSER_PROFILE_ENABLE"`
+      if [ $val -eq 0 ]; then
+        conf_enable ${mlnx_device} "FLEX_PARSER_PROFILE_ENABLE"
+        if [ $? -eq 0 ]; then
+          dev_reset ${mlnx_device}
+        fi
+      fi
+      ;;
+    *)
+      continue
+      ;;
+  esac
+done
 
 exec $@
