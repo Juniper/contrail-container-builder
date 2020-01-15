05227311601fa5dae3129ceac8b1301dcf5de4fb
Evgeny Shapovalov <eshapovalov.dev@gmail.com>
2020-01-15 20:38:36 +0300

    [Prototype] Fix problem with size for base container
    Change-Id: I75a5de36775847e2bfd020c3c40c919ef97e0de6

---
+++ containers/base/pre_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+BASE_EXTRA_RPMS=$(echo $BASE_EXTRA_RPMS | tr -d '"' | tr ',' ' ')
+if [[ -n "$BASE_EXTRA_RPMS" ]] ; then 
+    echo "INFO: contrail-base: install $BASE_EXTRA_RPMS"
+    yum install -y $BASE_EXTRA_RPMS
+    echo "INFO: importing gpg keys from any newly installed repos"
+    [ -d /etc/pki/rpm-gpg ] && rpm --import /etc/pki/rpm-gpg/*
+fi
