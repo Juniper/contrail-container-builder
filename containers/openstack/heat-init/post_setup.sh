4fd1ab38cd897b48883f4db7b49f2d2a1759b48c
Evgeny Shapovalov <eshapovalov.dev@gmail.com>
2020-01-15 20:38:36 +0300

    [Prototype] Fix problem with size for base container
    Change-Id: I75a5de36775847e2bfd020c3c40c919ef97e0de6

---
+++ containers/openstack/heat-init/post_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p /opt/contrail/site-packages
+cp -rf /usr/lib/python2.7/site-packages/contrail_heat* /opt/contrail/site-packages
+cp -rf /usr/lib/python2.7/site-packages/vnc_api* /opt/contrail/site-packages
+rm -rf /usr/lib/python2.7/site-packages/contrail_heat*
+rm -rf /usr/lib/python2.7/site-packages/vnc_api*
