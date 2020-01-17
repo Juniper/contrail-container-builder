83f33d401dcced48d0de7dd4dd348d4c250dea5c
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-01-17 14:07:33 +0300

    Add new provisioner container
    Change-Id: I9c12b9695f6e548534064f87a9924f786e1746b5

---
+++ containers/provisioner/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source /common.sh
+
+pre_start_init
+
+# Env variables:
+# NODE_TYPE = name of the component [vrouter, config, control, analytics, database, config-database, toragent]
+
+set_vnc_api_lib_ini
+
+if is_enabled ${MAINTENANCE_MODE} ; then
+  echo "WARNING: MAINTENANCE_MODE is switched on - provision.sh is not called."
+elif ! /provision.sh ; then
+  echo "ERROR: provision.sh was failed. Exiting..."
+  exit 1
+fi
