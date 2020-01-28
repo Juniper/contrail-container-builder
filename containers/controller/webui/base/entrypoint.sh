40f0b251276f53077308e730c482cbd1e14113a2
Biswajit Mandal <bmandal@juniper.net>
2020-01-28 01:50:34 -0800

    Adding domain info in userauth file.
    related-jira-bug: CEM-10414
    Change-Id: Iab1bdf006864588f75ce8193a9c543ef14f88b78

--- containers/controller/webui/base/entrypoint.sh
+++ containers/controller/webui/base/entrypoint.sh
@@ -271,6 +271,8 @@ auth.admin_user = '$KEYSTONE_AUTH_ADMIN_USER';
 auth.admin_password = '$KEYSTONE_AUTH_ADMIN_PASSWORD';
 auth.admin_token = '';
 auth.admin_tenant_name = '$KEYSTONE_AUTH_ADMIN_TENANT';
+auth.project_domain_name = '$KEYSTONE_AUTH_PROJECT_DOMAIN_NAME';
+auth.user_domain_name = '$KEYSTONE_AUTH_USER_DOMAIN_NAME';
 
 module.exports = auth;
 EOM
