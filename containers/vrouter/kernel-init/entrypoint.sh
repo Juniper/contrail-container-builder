0871baf74a1c8db3da3ee3f2eea658fdc9de70f6
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2020-01-30 17:32:49 +0300

    Update compute init scripts to pick vrouter.ko corresponding to the host kernel version
    Closes-Jira-Bug: CEM-11705
    Change-Id: I0d76f4d902159ff2a3403d191263c2ae8f9201d7

--- containers/vrouter/kernel-init/entrypoint.sh
+++ containers/vrouter/kernel-init/entrypoint.sh
@@ -5,13 +5,47 @@
 source /common.sh
 source /agent-functions.sh
 
+enable_kernel_module () {
+  mkdir -p /lib/modules/$1/kernel/net/vrouter
+  cp -f /opt/contrail/$1/vrouter.ko /lib/modules/$1/kernel/net/vrouter
+  depmod -a $1
+}
+
 copy_agent_tools_to_host
 
-modfile=$(find /opt/contrail/. -type f -name "vrouter.ko" | tail -1)
-for k_dir in `ls -d /lib/modules/*` ; do
-  mkdir -p ${k_dir}/kernel/net/vrouter
-  cp -f ${modfile} ${k_dir}/kernel/net/vrouter
-  depmod -a $(echo $k_dir | awk -F '/' '{print $NF}')
+# get versions vrouter.ko
+mod_dir=$(find /opt/contrail/. -type f -name "vrouter.ko" | awk  -F "/" '{print($(NF-1))}')
+# get installed kernels
+k_dir=$(ls /lib/modules)
+# get os & architecture
+os_arch=$(echo "$mod_dir" | tail -1 | awk -F '.' -v OFS='.' '{print ($(NF-1),($NF))}')
+# get latest version vrouter.ko
+latest_k_mod=$( echo "$mod_dir" | sed "s/\.$os_arch//" | sort -V | tail -n1 )
+# get kernels with versions higher than the latest vrouter.ko version
+# names truncation for correct "sort -V" execution
+high_k_vers=$( printf '%s\n' $k_dir $latest_k_mod | sed "s/\.$os_arch//" | sort -V | uniq | awk "/$latest_k_mod/ ? c++ : c" )
+
+# install vrouter.ko according to a specific kernel version
+for v in $mod_dir; do
+  short_v=$( echo $v | cut -d "." -f1,2,3 )
+  # Install completely identical kernel versions
+  if [[ "$k_dir" =~ "$v" ]]; then
+    enable_kernel_module "$v"
+  else
+    # Install vrouter.ko ignoring kernel fixes
+    if [[ "$k_dir" =~ "$short_v" ]]; then
+      enable_kernel_module "$v"
+    fi
+  fi
 done
+
+# installation of kernel modules for undefined previous kernel versions is skipped due to possible collisions
+
+# install the latest vrouter.ko to all higher kernel version
+if [[ -n "$high_k_vers" ]]; then
+  for k in $high_k_vers ; do
+    enable_kernel_module "${k}.${os_arch}"
+  done
+fi
 
 exec $@
