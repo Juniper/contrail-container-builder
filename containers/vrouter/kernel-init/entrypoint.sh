eed7c12e96b72cb2cec4e9d76cccb52dabfab27f
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2020-01-30 17:32:49 +0300

    Update compute init scripts to pick vrouter.ko corresponding to the host kernel version
    Closes-Jira-Bug: CEM-11705
    Change-Id: I0d76f4d902159ff2a3403d191263c2ae8f9201d7

--- containers/vrouter/kernel-init/entrypoint.sh
+++ containers/vrouter/kernel-init/entrypoint.sh
@@ -5,13 +5,37 @@
 source /common.sh
 source /agent-functions.sh
 
+enable_kernel_module () {
+  local s_dir="$1"
+  local d_dir="$2"
+  echo "Load vrouter.ko $s_dir for kernel $d_dir"
+  mkdir -p /lib/modules/$d_dir/kernel/net/vrouter
+  cp -f /opt/contrail/vrouter-kernel-modules/$s_dir/vrouter.ko /lib/modules/$d_dir/kernel/net/vrouter/
+  depmod -a $d_dir
+}
+
 copy_agent_tools_to_host
 
-modfile=$(find /opt/contrail/. -type f -name "vrouter.ko" | tail -1)
-for k_dir in `ls -d /lib/modules/*` ; do
-  mkdir -p ${k_dir}/kernel/net/vrouter
-  cp -f ${modfile} ${k_dir}/kernel/net/vrouter
-  depmod -a $(echo $k_dir | awk -F '/' '{print $NF}')
+mod_dir=$(find /opt/contrail/. -type f -name "vrouter.ko" | awk  -F "/" '{print($(NF-1))}')
+available_modules=( $( echo "$mod_dir" | sed 's/\.el/ el/' | sort -V | sed 's/ /./1') )
+printf 'Available vrouter.ko versions:'
+printf '%s\n' ${available_modules[@]}
+k_dir=$(find /lib/modules/. -type d -name "*.x86_64" | awk -F "/" '{print $NF}')
+installed_kernels=( $( printf  '%s\n' $k_dir ${m[@]} | sed 's/\.el/ el/' | sort -V | sed 's/ /./1' | uniq ) )
+printf 'Installed kernel versions:'
+printf '%s\n' $k_dir
+
+i=$((${#available_modules[@]}-1))
+for r in $(printf  '%s\n' ${installed_kernels[@]} | tac) ; do
+  [[ "$r" == "${available_modules[$i]}" ]] && ((i--))
+done
+((i++))
+
+offset=0
+for l in ${installed_kernels[@]} ; do
+  [[ "$l" == "${available_modules[$i]}" ]] && [[ "$i" != "$((${#available_modules[@]}-1))" ]] && ((i++)) && offset=0
+  [[ "$l" == "${installed_kernels[0]}" && "$l" == ${available_modules[$(($i-1))]} ]] &&  offset=-1
+  [[ -d "/lib/modules/${k}" ]] && enable_kernel_module "${available_modules[$(($i+$offset))]}" "$l"
 done
 
 exec $@
