1ae65fb34fda15819c29486d2431ce4219c3f94d
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2020-03-16 13:39:42 +0300

    Fix the picking of the incorrect kernel
    Closes-Jira-Bug: CEM-13621
    Change-Id: I2bde36b8bc389a45068e5754d20a4587e446ec44

--- containers/vrouter/kernel-init/entrypoint.sh
+++ containers/vrouter/kernel-init/entrypoint.sh
@@ -17,25 +17,24 @@ enable_kernel_module () {
 copy_agent_tools_to_host
 
 mod_dir=$(find /opt/contrail/. -type f -name "vrouter.ko" | awk  -F "/" '{print($(NF-1))}')
-available_modules=( $( echo "$mod_dir" | sed 's/\.el/ el/' | sort -V | sed 's/ /./1') )
+available_modules=$(echo "$mod_dir" | sed 's/\.el/ el/' | sort -V | sed 's/ /./1')
 printf 'Available vrouter.ko versions:\n'
 printf '%s\n' ${available_modules[@]}
 k_dir=$(find /lib/modules/. -type d -name "*.x86_64" | awk -F "/" '{print $NF}')
-kernels_list=( $( printf  '%s\n' $k_dir ${available_modules[@]} | sed 's/\.el/ el/' | sort -V | sed 's/ /./1' | uniq ) )
 printf 'Installed kernel versions:\n'
 printf '%s\n' $k_dir
 
-i=$((${#available_modules[@]}-1))
-for r in $(printf  '%s\n' ${kernels_list[@]} | tac) ; do
-  [[ "$r" == "${available_modules[$i]}" ]] && ((i--))
-done
-((i++))
-
-offset=0
-for l in ${kernels_list[@]} ; do
-  [[ "$l" == "${available_modules[$i]}" ]] && [[ "$i" != "$((${#available_modules[@]}-1))" ]] && ((i++)) && offset=0
-  [[ "$l" == "${kernels_list[0]}" && "$l" == ${available_modules[$(($i-1))]} ]] &&  offset=-1
-  [[ -d "/lib/modules/${l}" ]] && enable_kernel_module "${available_modules[$(($i+$offset))]}" "$l"
+for d in $k_dir ; do
+  if echo "$available_modules" | grep -q "$d" ; then
+    enable_kernel_module "$d" "$d"
+    continue
+  fi
+  s=$(echo -e "${available_modules}\n${d}" | sed 's/\.el/ el/' | sort -V | sed 's/ /./1')
+  if ! echo "$s" | grep -A1 "$d" | grep -vq "$d" ; then
+    enable_kernel_module "${available_modules##*$'\n'}" "$d"
+    continue
+  fi
+  enable_kernel_module $(echo "$s" | grep -A1 "$d" | grep -v "$d") "$d"
 done
 
 exec $@
