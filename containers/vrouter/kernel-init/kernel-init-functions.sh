495bb4fcbeeeb6abf0861277bad3ffe808ec7b09
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-03-19 15:52:30 +0300

    Add unit-test for kernel-init container
    Related-jira-bug: CEM-13621
    Change-Id: I682b8acf56153b700b1cd7b49bf2dfd0a92ae2a5

---
+++ containers/vrouter/kernel-init/kernel-init-functions.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+enable_kernel_module () {
+  local s_dir="$1"
+  local d_dir="$2"
+  echo "Load vrouter.ko $s_dir for kernel $d_dir"
+  mkdir -p /lib/modules/$d_dir/kernel/net/vrouter
+  cp -f /opt/contrail/vrouter-kernel-modules/$s_dir/vrouter.ko /lib/modules/$d_dir/kernel/net/vrouter/
+  depmod -a $d_dir
+}
+
+get_vrouter_dirs () {
+  local path=$1
+  find "$path" -type f -name "vrouter.ko"
+}
+
+get_kernel_dirs () {
+  local path=$1
+  find "$path" -type d -name "*.x86_64"
+}
+
+get_lists_modules_versions () {
+  local list_dirs=$1
+  echo "$list_dirs" | awk -F "/" '{print($(NF-1))}' | sed 's/\.el/ el/' | sort -V | sed 's/ /./1'
+}
+
+get_lists_kernels_versions () {
+  local list_dirs=$1
+  echo "$list_dirs_kernels" | awk -F "/" '{print $NF}'
+}
+
+install_kernel_modules () {
+  local modules=$1
+  local kernels=$2
+  local sorted_list
+  for d in $kernels ; do
+    # Enable module if we have equal version
+    if echo "$modules" | grep -q "$d" ; then
+      enable_kernel_module "$d" "$d"
+      continue
+    fi
+    # Add OS kernel version to list of available and sort them
+    sorted_list=$(echo -e "${modules}\n${d}" | sed 's/\.el/ el/' | sort -V | sed 's/ /./1')
+    if ! echo "$sorted_list" | grep -B1 "$d" | grep -vq "$d" ; then
+      # Enable first installed module if current kernel is upper all modules that we have
+      enable_kernel_module $(echo "$modules" | head -1) "$d"
+    else
+      # Enable upper version kernel module
+      enable_kernel_module $(echo "$sorted_list" | grep -B1 "$d" | grep -v "$d") "$d"
+    fi
+  done
+}
---
+++ run-tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+set -o pipefail
+
+[ "${DEBUG,,}" == "true" ] && set -x
+scriptdir=$(realpath $(dirname "$0"))
+
+repo_path=${repo_path:-"$scriptdir"}
+logs_path=${logs_path:-"${repo_path}/logs"}
+reports_dir=${reports_dir:-"${repo_path}/logs"}
+mkdir -p "$logs_path"
+mkdir -p "$reports_dir"
+
+for unit_test in $( find "$repo_path" -type f -name "run-test.sh" ) ; do
+  container_name=$( echo $unit_test | awk -F "/" '{print($(NF-3))}' )
+  $unit_test 2>&1 | tee "${reports_dir}/${container_name}.log" || FAILED_TESTS+=($container_name)
+done
+
+if [ "${#FAILED_TESTS[@]}" -ne '0' ] ; then
+  echo "ERROR: Tests failed for containers:"
+  for t in ${FAILED_TESTS[*]} ; do
+    echo "$t"
+  done
+  echo "INFO: Units tests log is available at $logs_path"
+  exit 1
+else
+  echo "INFO: All containers tests are successful"
+fi
