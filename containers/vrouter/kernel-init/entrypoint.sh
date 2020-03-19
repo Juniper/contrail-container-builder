a7dfd189072ba508131f2c59632b60887e02bd4b
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-03-19 15:52:30 +0300

    Add unit-test for kernel-init container
    Related-jira-bug: CEM-13621
    Change-Id: I682b8acf56153b700b1cd7b49bf2dfd0a92ae2a5

--- containers/vrouter/kernel-init/entrypoint.sh
+++ containers/vrouter/kernel-init/entrypoint.sh
@@ -4,43 +4,12 @@
 
 source /common.sh
 source /agent-functions.sh
-
-enable_kernel_module () {
-  local s_dir="$1"
-  local d_dir="$2"
-  echo "Load vrouter.ko $s_dir for kernel $d_dir"
-  mkdir -p /lib/modules/$d_dir/kernel/net/vrouter
-  cp -f /opt/contrail/vrouter-kernel-modules/$s_dir/vrouter.ko /lib/modules/$d_dir/kernel/net/vrouter/
-  depmod -a $d_dir
-}
+source /kernel-init-functions.sh
 
 copy_agent_tools_to_host
 
-# Collect kernel module versions from package
-mod_dir=$(find /opt/contrail/. -type f -name "vrouter.ko" | awk  -F "/" '{print($(NF-1))}')
-available_modules=$(echo "$mod_dir" | sed 's/\.el/ el/' | sort -V | sed 's/ /./1')
-echo "Available vrouter.ko versions:"
-echo "$available_modules"
-# Collect installed kernels from system
-installed_kernels=$(find /lib/modules/. -type d -name "*.x86_64" | awk -F "/" '{print $NF}')
-echo "Installed kernel versions:"
-echo "$installed_kernels"
-
-for d in $installed_kernels ; do
-  # Enable module if we have equal version
-  if echo "$available_modules" | grep -q "$d" ; then
-    enable_kernel_module "$d" "$d"
-    continue
-  fi
-  # Add OS kernel version to list of available and sort them
-  sorted_list=$(echo -e "${available_modules}\n${d}" | sed 's/\.el/ el/' | sort -V | sed 's/ /./1')
-  if ! echo "$sorted_list" | grep -B1 "$d" | grep -vq "$d" ; then
-    # Enable first installed module if current kernel is upper all modules that we have
-    enable_kernel_module $(echo "$available_modules" | head -1) "$d"
-  else
-    # Enable upper version kernel module
-    enable_kernel_module $(echo "$sorted_list" | grep -B1 "$d" | grep -v "$d") "$d"
-  fi
-done
+get_dirs_lists
+get_lists_versions
+install_kernel_modules
 
 exec $@
---
+++ containers/vrouter/kernel-init/tests/run.sh
@@ -0,0 +1,62 @@
+#!/bin/bash -xeET
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+source "$my_dir/../kernel-init-functions.sh"
+
+# Define the dataset for testing
+list_dirs_modules="/opt/contrail/vrouter-kernel-modules/3.10.0-229.el7.x86_64/vrouter.ko
+/opt/contrail/vrouter-kernel-modules/3.10.0-514.el7.x86_64/vrouter.ko
+/opt/contrail/vrouter-kernel-modules/3.10.0-1062.1.1.el7.x86_64/vrouter.ko
+/opt/contrail/vrouter-kernel-modules/3.10.0-1062.9.1.el7.x86_64/vrouter.ko"
+
+list_dirs_kernels="/lib/modules/3.10.0-327.el7.x86_64
+/lib/modules/3.10.0-1062.el7.x86_64
+/lib/modules/3.10.0-1062.1.1.el7.x86_64
+/lib/modules/3.10.0-1062.1.2.el7.x86_64
+/lib/modules/3.10.0-1062.7.1.el7.x86_64
+/lib/modules/3.10.0-693.el7.x86_64
+/lib/modules/3.10.0-862.el7.x86_64
+/lib/modules/3.10.0-957.el7.x86_64
+/lib/modules/3.10.0-1062.9.1.el7.x86_64
+/lib/modules/3.10.0-1062.12.1.el7.x86_64"
+
+declare -A RESULTS
+unset -f enable_kernel_module
+enable_kernel_module () {
+  local s_dir="$1"
+  local d_dir="$2"
+  echo "For kernel $2 module $1 is proposed"
+  RESULTS[$2]="$1"
+}
+
+# Running tested features
+get_lists_versions
+install_kernel_modules
+
+# Check result
+
+[[ "${RESULTS["3.10.0-327.el7.x86_64"]}" == '3.10.0-229.el7.x86_64' ]] || FAILED_KERNELS+=('3.10.0-327.el7.x86_64')
+[[ "${RESULTS["3.10.0-693.el7.x86_64"]}" == '3.10.0-514.el7.x86_64' ]] || FAILED_KERNELS+=('3.10.0-693.el7.x86_64')
+[[ "${RESULTS["3.10.0-862.el7.x86_64"]}" == '3.10.0-514.el7.x86_64' ]] || FAILED_KERNELS+=('3.10.0-862.el7.x86_64')
+[[ "${RESULTS["3.10.0-957.el7.x86_64"]}" == '3.10.0-514.el7.x86_64' ]] || FAILED_KERNELS+=('3.10.0-957.el7.x86_64')
+[[ "${RESULTS["3.10.0-1062.el7.x86_64"]}" == '3.10.0-514.el7.x86_64' ]] || FAILED_KERNELS+=('3.10.0-1062.el7.x86_64')
+[[ "${RESULTS["3.10.0-1062.1.1.el7.x86_64"]}" == '3.10.0-1062.1.1.el7.x86_64' ]] || FAILED_KERNELS+=('3.10.0-1062.1.1.el7.x86_64')
+[[ "${RESULTS["3.10.0-1062.1.2.el7.x86_64"]}" == '3.10.0-1062.1.1.el7.x86_64' ]] || FAILED_KERNELS+=('3.10.0-1062.1.2.el7.x86_64')
+[[ "${RESULTS["3.10.0-1062.7.1.el7.x86_64"]}" == '3.10.0-1062.1.1.el7.x86_64' ]] || FAILED_KERNELS+=('3.10.0-1062.7.1.el7.x86_64')
+[[ "${RESULTS["3.10.0-1062.9.1.el7.x86_64"]}" == '3.10.0-1062.9.1.el7.x86_64' ]] || FAILED_KERNELS+=('3.10.0-1062.9.1.el7.x86_64')
+[[ "${RESULTS["3.10.0-1062.12.1.el7.x86_64"]}" == '3.10.0-1062.9.1.el7.x86_64' ]] || FAILED_KERNELS+=('3.10.0-1062.12.1.el7.x86_64')
+
+# Report
+set +x
+if [ "${#FAILED_KERNELS[@]}" -ne '0' ] ; then
+  echo "TEST FAILED!"
+  echo "An inappropriate module is proposed for kernels:"
+  for i in ${FAILED_KERNELS[*]} ; do
+    echo "$i"
+  done
+  exit 1
+else
+  echo "TEST SUCCESS"
+fi
