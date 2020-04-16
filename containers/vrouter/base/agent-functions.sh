c2c01e20b1c6ba6abc629517f79f7a137fa8b76b
alexey-mr <alexey.morlang@gmail.com>
2020-04-17 00:12:31 +0300

    Check and reload vrouter if if is changed
    close-jira-bug: CEM-14748
    Change-Id: Ib5e2fbb0a5a95a504d3f5a4366d72d3465d3d028

--- containers/vrouter/base/agent-functions.sh
+++ containers/vrouter/base/agent-functions.sh
@@ -510,14 +510,57 @@ function ensure_host_resolv_conf() {
     fi
 }
 
+function get_loaded_vrouter_version() {
+    cat /sys/module/vrouter/version
+}
+
+function get_available_vrouter_version() {
+    modinfo -F version  vrouter
+}
+
+function match_cur_and_avail_vrouter_vers() {
+    local cur_ver=$(get_loaded_vrouter_version)
+    local avail_ver=$(get_available_vrouter_version)
+    [[ "$cur_ver" == "$avail_ver" ]]
+}
+
+function match_build_and_avail_vrouter_vers() {
+    local avail_ver=$(get_available_vrouter_version)
+    echo "INFO: vrouter avail_ver=$avail_ver"
+    [[ "$BUILD_VERSION" == "$avail_ver" ]]
+}
+
+function wait_vrouter_match_agent_build_version() {
+    if [[ -z "$BUILD_VERSION" ]] ; then
+        echo "DEBUG: not BUILD_VERSION provided to sync with kernel installation"
+        return
+    fi
+    echo "INFO: wait till vrouter available for BUILD_VERSION=$BUILD_VERSION"
+    wait_cmd_success match_build_and_avail_vrouter_vers 5 30
+}
+
 function init_vhost0() {
     # Probe vhost0
     local vrouter_cidr="$(get_cidr_for_nic vhost0)"
     if [[ "$vrouter_cidr" != '' ]] ; then
         echo "INFO: vhost0 is already up"
-        ensure_host_resolv_conf
-        return 0
+        if is_dpdk || match_cur_and_avail_vrouter_vers ; then
+            ensure_host_resolv_conf
+            return 0
+        fi
+        if [[ ! -z "$BIND_INT" ]] ; then
+            # do not reinit in container in case if called in tripleo
+            # this case should not happen normally, just log it
+            echo "DEBUG: attempt to call init_vhost0 from ifup from host - wrong usage"
+            return 0
+        fi
+        echo "INFO: vrouter changed.. ifdown vhost0 to reload"
+        remove_vhost0
     fi
+    # Upgrade case to wait appropriate vrouter module (on ubuntu it takes time to build new
+    # and agent is run before the build if done)
+    echo "INFO: wait till uptodate vrouter module available"
+    wait_vrouter_match_agent_build_version || return 1
 
     declare phys_int phys_int_mac addrs bind_type bind_int mtu routes
     if ! is_dpdk ; then
