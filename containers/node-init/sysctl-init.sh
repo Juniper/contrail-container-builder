d78ccff4e31bac6af074ed9ac901c0661d0c6df9
Andrey Pavlov <andrey-mp@yandex.ru>
2020-06-08 17:47:04 +0300

    [Migration/Juniper/contrail-container-builder] Remove content and add readme about migration.
    Automated change
    
    Depends-On: I1cdab6adf1e51e5b407b2d90456d596e48557582
    Depends-On: Ibbb65ac2f7c522b0f4ffcdda2051b9b61fc6acab
    Depends-On: Ic41e6f42553fe6bab6d25946345d83f6c731ddbd
    Change-Id: I737a201563e58bc067a1e6d2ec22aecb9431b620

--- containers/node-init/sysctl-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source /common.sh
-
-# configure core_pattern for Host OS
-set_ctl kernel.core_pattern /var/crashes/core.%e.%p.%h.%t
--- containers/vrouter/agent/post_setup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm -rf /etc/sysconfig/network-scripts
