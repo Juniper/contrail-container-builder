febe2b7cf7318b1ed05ae5ea4f49e20fd9db9b24
alexey-mr <alexey.morlang@gmail.com>
2020-06-08 15:24:58 +0300

    Added python3 related packages for neutron plugin
    partial-jira-bug: CEM-11347
    Change-Id: Ie35bf2aab92a111838d1d38184dc8cb019c3434c

--- containers/openstack/heat-init/entrypoint.sh
+++ containers/openstack/heat-init/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -ex
 
-mkdir -p /opt/plugin/site-packages
+mkdir -p /opt/plugin/site-packages /opt/plugin/python3/site-packages
+
+# python3
+cp -rf /opt/contrail/python3/site-packages/* /opt/plugin/python3/site-packages
+cp -rf /opt/contrail/site-packages/* /opt/plugin/python3/site-packages
+
+# python2
+cp -rf /opt/contrail/python2/site-packages/* /opt/plugin/site-packages/
 cp -rf /opt/contrail/site-packages/* /opt/plugin/site-packages/
