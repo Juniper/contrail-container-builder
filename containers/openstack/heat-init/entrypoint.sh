998f80c89159767e8f11efcfe9186b71921c615a
alexey-mr <alexey.morlang@gmail.com>
2020-06-08 15:24:58 +0300

    Added python3 related packages for neutron plugin
    partial-jira-bug: CEM-11347
    Change-Id: Ie35bf2aab92a111838d1d38184dc8cb019c3434c

--- containers/openstack/heat-init/entrypoint.sh
+++ containers/openstack/heat-init/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -ex
 
-mkdir -p /opt/plugin/site-packages
+mkdir -p /opt/plugin/site-packages /opt/plugin/python3/site-packages
+
+# python3
+cp -rf /opt/contrail_python3/site-packages/* /opt/plugin/python3/site-packages/
+
+# python2
 cp -rf /opt/contrail/site-packages/* /opt/plugin/site-packages/
--- containers/openstack/neutron-init/entrypoint.sh
+++ containers/openstack/neutron-init/entrypoint.sh
@@ -20,7 +20,12 @@ function copy_sources() {
   done
 }
 
-mkdir -p /opt/plugin/site-packages
+mkdir -p /opt/plugin/site-packages /opt/plugin/python3/site-packages
+
+# python3
+cp -rf /opt/contrail_python3/site-packages/* /opt/plugin/python3/site-packages/
+
+# python2
 cp -rf /opt/contrail/site-packages/* /opt/plugin/site-packages/
 
 # install appropriate version of python-neutron-lbaas based on OPENSTACK_VERSION
